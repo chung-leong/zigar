1fdf5d5ce7921660a1202863ec69a68b8955d1a5
Chung Leong <chernyshevsky@hotmail.com>
2023-05-28 00:01:01 +0200

    Initial work on new JS-based approach

---
+++ test/js/dataview.test.js
@@ -0,0 +1,91 @@
+import { expect } from 'chai';
+
+import { defineStruct } from '../../src/js/dataview.js';
+
+const Pointer = 0;
+const Bool = 1;
+const Int = 2;
+const Float = 3;
+
+describe("JavaScript Struct DataView creation", function() { 
+  it('should create a class()', function() {
+    const fields = {
+        dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+        cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const Hello = defineStruct('Hello', 8, fields);
+    expect(Hello).to.be.a('function');
+  })
+  it('should return default values', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const Hello = defineStruct('Hello', 8, fields);
+    const obj = new Hello();
+    expect(obj.dog).to.be.equal(43);
+    expect(obj.cat).to.be.equal(3332);
+  })
+  it('should have dataView property when exposeDataView is true', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: true });
+    const obj = new Hello();
+    expect(obj.dataView).to.be.instanceOf(DataView);
+  })
+  it('should have dataView property when exposeDataView is false', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: false });
+    const obj = new Hello();
+    expect(obj.dataView).to.be.undefined;
+  })
+  it('should change underlying ArrayBuffer property is set', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: true });
+    const obj = new Hello();
+    obj.dog = 4567;
+    expect(obj.dataView.getInt32(0, true)).to.equal(4567);
+  })
+  it('should accept an ArrayBuffer as constructor argument', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const buffer = new ArrayBuffer(8);
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: true });
+    const obj = new Hello(buffer);
+    obj.dog = 14567;
+    const dataView = new DataView(buffer);
+    expect(dataView.getInt32(0, true)).to.equal(14567);
+  })
+  it('should accept a DataView as constructor argument', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const buffer = new ArrayBuffer(8);
+    const dataView = new DataView(buffer);
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: true });
+    const obj = new Hello(dataView);
+    obj.dog = 14567;
+    expect(dataView.getInt32(0, true)).to.equal(14567);
+  })
+  it('should throw when array buffer is not the correct size', function() {
+    const fields = {
+      dog: { type: Int, bits: 32, offset: 0, signed: true, bitOffset: 0, defaultValue: 43, writable: true },
+      cat: { type: Int, bits: 32, offset: 4, signed: true, bitOffset: 0, defaultValue: 3332 },
+    };
+    const buffer = new ArrayBuffer(9);
+    const Hello = defineStruct('Hello', 8, fields, { exposeDataView: true });
+    expect(() => new Hello(buffer)).to.throw();
+  })
+
+})
