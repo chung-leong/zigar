a4ae98631fe7b2565dabf18d0332db855b2ef623
Chung Leong <chernyshevsky@hotmail.com>
2023-06-09 13:26:13 +0200

    Work in progress

--- src/host.zig
+++ src/host.zig
@@ -40,6 +40,12 @@ pub const Member = extern struct {
     bits: u32,
     signed: bool = false,
     len: u32 = 0,
+    class_id: u32,
+};
+
+pub const Memory = extern struct {
+    bytes: [*]u8,
+    len: usize,
 };
 
 pub const Value = *opaque {};
@@ -51,61 +57,94 @@ const Callbacks = extern struct {
     allocate_memory: *const fn (host: Host, size: usize, dest: *[*]u8) callconv(.C) Result,
     reallocate_memory: *const fn (host: Host, size: usize, dest: *[*]u8) callconv(.C) Result,
     free_memory: *const fn (host: Host, dest: *[*]u8) callconv(.C) Result,
+    get_memory: *const fn (host: Host, value: Value, dest: *Memory) callconv(.C) Result,
+    get_relocatable: *const fn (host: Host, value: Value, id: u32, dest: *Value) callconv(.C) Result,
 
-    get_slot: *const fn (host: Host, slot_id: usize, dest: *Value) callconv(.C) Result,
-    set_slot: *const fn (host: Host, slot_id: usize, object: Value) callconv(.C) Result,
+    get_slot: *const fn (host: Host, id: u32, dest: *Value) callconv(.C) Result,
+    set_slot: *const fn (host: Host, id: u32, value: Value) callconv(.C) Result,
 
     begin_structure: *const fn (host: Host, s_type: StructureType, dest: *Value) callconv(.C) Result,
-    add_member: *const fn (host: Host, def: Value, member: Member) callconv(.C) Result,
-    finalize_structure: *const fn (host: Host, def: Value, dest: *Value) callconv(.C) Result,
+    attach_default_data: *const fn (host: Host, def: Value, bytes: Memory) callconv(.C) Result,
+    attach_default_pointer: *const fn (host: Host, def: Value, id: u32, reloc: Value) callconv(.C) Result,
+    attach_member: *const fn (host: Host, def: Value, member: Member) callconv(.C) Result,
+    attach_static: *const fn (host: Host, def: Value, static: Value) callconv(.C) Result,
+    finalize_structure: *const fn (host: Host, def: Value) callconv(.C) Result,
 };
 var callbacks: Callbacks = undefined;
 
 pub const Host = *opaque {
     // allocate slots for classe, function, and other language constructs on the host side
-    const type_slot = slot.allocator.get(.{ .type = "host_type" });
+    const type_slot = slot.allocator.get(.{});
 
-    fn getConstruct(self: Host, comptime S: anytype) Value {
-        const slot_id = type_slot.get(S);
-        var construct: Value = undefined;
-        if (callbacks.get_slot(self, slot_id, &construct) != .OK) {
-            return null;
+    fn getPointer(self: Host, value: Value, comptime PT: type) !PT {
+        var memory: Memory = undefined;
+        if (callbacks.get_memory(self, value, &memory) != .OK) {
+            return Error.UnknownError;
         }
-        return construct;
+        return @ptrCast(PT, memory.bytes);
     }
 
-    fn setConstruct(self: Host, comptime S: anytype, construct: Value) !void {
-        const slot_id = type_slot.get(S);
-        if (callbacks.set_slot(self, slot_id, construct) != .OK) {
+    fn getRelocatable(self: Host, value: Value, id: u32) Value {
+        var result: Value = undefined;
+        if (callbacks.get_relocatable(self, value, id, &result) != .OK) {
+            return Error.UnknownError;
+        }
+        return result;
+    }
+
+    fn getConstructId(_: Host, comptime S: anytype) u32 {
+        return type_slot.get(S);
+    }
+
+    fn getRelocatableId(_: Host, comptime T: anytype, index: comptime_int) u32 {
+        // per-struct slot allocator
+        const relocatable_slot = slot.allocator.get(.{ .Struct = T });
+        return relocatable_slot.get(.{ .Index = index });
+    }
+
+    fn getSlot(self: Host, id: u32) !Value {
+        var value: Value = undefined;
+        if (callbacks.get_slot(self, id, &value) != .OK) {
+            return Error.UnknownError;
+        }
+        return value;
+    }
+
+    fn setSlot(self: Host, id: u32, value: Value) !void {
+        if (callbacks.set_slot(self, id, value) != .OK) {
             return Error.UnknownError;
         }
     }
 
     fn beginStructure(self: Host, s_type: StructureType) !Value {
-        var value: Value = undefined;
-        if (callbacks.begin_structure(self, s_type, &value) != .OK) {
+        var def: Value = undefined;
+        if (callbacks.begin_structure(self, s_type, &def) != .OK) {
             return Error.UnknownError;
         }
-        return value;
+        return def;
     }
 
-    fn addMember(self: Host, def: Value, member: Member) !void {
-        if (callbacks.add_member(self, def, member) != .OK) {
+    fn attachMember(self: Host, def: Value, member: Member) !void {
+        if (callbacks.attach_member(self, def, member) != .OK) {
             return Error.UnknownError;
         }
     }
 
-    fn finalizeStructure(self: Host, def: Value) Value {
-        var value: Value = undefined;
-        if (callbacks.finalize_structure(self, def, &value) != .OK) {
+    fn attachStatic(self: Host, def: Value, static: Value) !void {
+        if (callbacks.attach_static(self, def, static) != .OK) {
             return Error.UnknownError;
         }
-        return value;
+    }
+
+    fn finalizeStructure(self: Host, def: Value) !void {
+        if (callbacks.finalize_structure(self, def) != .OK) {
+            return Error.UnknownError;
+        }
     }
 };
 
 pub const Thunk = *const fn (host: Host) callconv(.C) void;
-pub const Factory = *const fn (host: Host, dest: *Value) callconv(.C) Result;
+pub const Factory = *const fn (host: Host, dest: *u32) callconv(.C) Result;
 pub const Module = extern struct {
     version: c_int,
     callbacks: *Callbacks,
