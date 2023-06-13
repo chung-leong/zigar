dbb8c08d2b8481b27c4cfd6c261ec9481eb21cfc
Chung Leong <chernyshevsky@hotmail.com>
2023-06-13 04:21:04 +0200

    Enabled JS miniification (#11).

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,25 @@
+import Terser from '@rollup/plugin-terser';
+
 export default {
   input: 'src/define.js',
   output: {
 		file: 'src/addon.js.txt',
 		format: 'iife',
-    banner: 'R"=====(',
-    footer: ')====="',
+    plugins: [ 
+      Terser(),
+      {
+        // turn code into C++ raw string 
+        // we can't rely on banner/footer here since those are seen by Terser, 
+        // which would prompt throw an syntax error
+        name: 'C++ string',
+        renderChunk (code) {
+          return `R"=====(\n${code}\n)====="`;
+        }
+      },
+    ]
 	},
   onwarn(warning, warn) {
-    if (warning.code === 'EVAL') return
+    if (warning.code === 'MISSING_NAME_OPTION_FOR_IIFE_EXPORT') return
     warn(warning)
-  }  
+  }   
 };
