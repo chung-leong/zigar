e1b2f1fed047beaee530889010970ca870f71eff
Chung Leong <chernyshevsky@hotmail.com>
2023-06-18 17:02:11 +0200

    Got minification to work (#11)

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ export default {
   output: {
 		file: 'src/addon.js.txt',
 		format: 'iife',
+    name: 'variable',
     plugins: [ 
       minify ? Terser() : undefined,
       {
@@ -15,13 +16,11 @@ export default {
         // which would promptly throw a syntax error
         name: 'C++ string',
         renderChunk (code) {
+          // Terser insists on saving result to variable--remove assignment
+          code = code.replace(/var variable\s*=\s*([\s\S]*);/, '($1)');
           return `R"=====(\n${code}\n)====="`;
         }
       },
     ]
 	},
-  onwarn(warning, warn) {
-    if (warning.code === 'MISSING_NAME_OPTION_FOR_IIFE_EXPORT') return
-    warn(warning)
-  }   
 };
