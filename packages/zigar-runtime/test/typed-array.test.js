17d371675ceec3e8f38c014fff17fcd9c7c077c3
Chung Leong <chernyshevsky@hotmail.com>
2023-08-01 23:37:28 +0200

    Full coverage for typed-array.js (#78)

--- packages/zigar-runtime/test/typed-array.test.js
+++ packages/zigar-runtime/test/typed-array.test.js
@@ -7,17 +7,32 @@ import {
 } from '../src/typed-array.js';
 
 describe('Typed array functions', function() {
-  describe('getTypedArrayGetter', function() {
+  describe('getTypedArrayClass', function() {
     it('should a typed array class when element type is standard', function() {
-      const member = {
-        type: MemberType.Int,
-        isSigned: true,
-        bitSize: 32,
-        byteSize: 4,
-      };
-      const f = getTypedArrayClass(member);
-      expect(f).to.be.a('function');
-      expect(f).to.equal(Int32Array);
+      let index = 0;
+      const types = [
+        Int8Array,
+        Uint8Array,
+        Int16Array,
+        Uint16Array,
+        Int32Array,
+        Uint32Array,
+        BigInt64Array,
+        BigUint64Array,
+      ];
+      for (const byteSize of [ 1, 2, 4, 8 ]) {
+        for (const isSigned of [ true, false ]) {
+          const member = {
+            type: MemberType.Int,
+            isSigned,
+            bitSize: byteSize * 8,
+            byteSize,
+          };
+          const f = getTypedArrayClass(member);
+          expect(f).to.be.a('function');
+          expect(f).to.equal(types[index++]);
+        }
+      }
     })
     it('should a typed array class that fits the non-standard type', function() {
       const member = {
@@ -28,6 +43,23 @@ describe('Typed array functions', function() {
       };
       const f = getTypedArrayClass(member);
       expect(f).to.equal(BigUint64Array);
+    })
+    it('should a typed array class for floating point numbers', function() {
+      let index = 0;
+      const types = [
+        Float32Array,
+        Float64Array,
+      ];
+      for (const byteSize of [ 4, 8 ]) {
+        const member = {
+          type: MemberType.Float,
+          isSigned: false,
+          bitSize: byteSize * 8,
+          byteSize,
+        };
+        const f = getTypedArrayClass(member);
+        expect(f).to.equal(types[index++]);
+      }
     })
   })
   describe('isTypedArray', function() {
