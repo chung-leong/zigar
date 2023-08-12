02f7090de40dfadbc3aa4bfaa644e42544281d4a
Chung Leong <chernyshevsky@hotmail.com>
2023-08-12 18:18:27 +0200

    Added n-body example (#104)

--- zigar-compiler/test/integration.js
+++ zigar-compiler/test/integration.js
@@ -194,6 +194,77 @@ export function addTests(importModule, options) {
       expect([ ...d ]).to.eql([ 6, 8, 10, 12 ]);
     })
   })
+  describe('ZIG Benchmarks Game', function() {
+    it('should produce the right results for the nbody example', async function() {
+      this.timeout(60000);
+      const {
+        default: {
+          Planets, solar_mass, year, advance, energy, offset_momentum
+        }
+      } = await importModule(resolve('./zig-samples/benchmarks-game/nbody.zig'));
+      const solar_bodies = new Planets([
+        // Sun
+        {
+          x: 0.0,
+          y: 0.0,
+          z: 0.0,
+          vx: 0.0,
+          vy: 0.0,
+          vz: 0.0,
+          mass: solar_mass,
+        },
+        // Jupiter
+        {
+            x: 4.84143144246472090e+00,
+            y: -1.16032004402742839e+00,
+            z: -1.03622044471123109e-01,
+            vx: 1.66007664274403694e-03 * year,
+            vy: 7.69901118419740425e-03 * year,
+            vz: -6.90460016972063023e-05 * year,
+            mass: 9.54791938424326609e-04 * solar_mass,
+        },
+        // Saturn
+        {
+            x: 8.34336671824457987e+00,
+            y: 4.12479856412430479e+00,
+            z: -4.03523417114321381e-01,
+            vx: -2.76742510726862411e-03 * year,
+            vy: 4.99852801234917238e-03 * year,
+            vz: 2.30417297573763929e-05 * year,
+            mass: 2.85885980666130812e-04 * solar_mass,
+        },
+        // Uranus
+        {
+            x: 1.28943695621391310e+01,
+            y: -1.51111514016986312e+01,
+            z: -2.23307578892655734e-01,
+            vx: 2.96460137564761618e-03 * year,
+            vy: 2.37847173959480950e-03 * year,
+            vz: -2.96589568540237556e-05 * year,
+            mass: 4.36624404335156298e-05 * solar_mass,
+        },
+        // Neptune
+        {
+            x: 1.53796971148509165e+01,
+            y: -2.59193146099879641e+01,
+            z: 1.79258772950371181e-01,
+            vx: 2.68067772490389322e-03 * year,
+            vy: 1.62824170038242295e-03 * year,
+            vz: -9.51592254519715870e-05 * year,
+            mass: 5.15138902046611451e-05 * solar_mass,
+        },
+      ]);
+      const n = 50000;
+      offset_momentum(solar_bodies);
+      const result1 = energy(solar_bodies['*']);
+      advance(solar_bodies, 0.01, n);
+      const result2 = energy(solar_bodies['*']);
+      expect(year).to.equal(365.24);
+      expect(solar_mass).to.equal(4.0 * Math.PI * Math.PI);
+      expect(result1).to.equal(-0.169075164);
+      expect(result2).to.equal(-0.169078071);
+    })
+  })
 }
 
 function resolve(relPath) {
