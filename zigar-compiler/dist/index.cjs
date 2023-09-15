chung-leong/zigar

commit 763623a131c80b488a03891033c5c004ecc98aec

Refactored out isSigned (#192)

zigar-runtime/src/member.js
@@ -4,6 +4,8 @@ import {
   getDataViewBoolAccessorEx,
   getDataViewIntAccessor,
   getDataViewIntAccessorEx,
+  getDataViewUintAccessor,
+  getDataViewUintAccessorEx,
   getDataViewFloatAccessor,
   getDataViewFloatAccessorEx,
 } from './data-view.js';
@@ -22,11 +24,12 @@ export const MemberType = {
   Void: 0,
   Bool: 1,
   Int: 2,
-  Float: 3,
-  EnumerationItem: 4,
-  Object: 5,
-  Type: 6,
-  Comptime: 7,
+  Uint: 3,
+  Float: 4,
+  EnumerationItem: 5,
+  Object: 6,
+  Type: 7,
+  Comptime: 8,
 };
 
 const factories = Array(Object.values(MemberType).length);
@@ -49,6 +52,14 @@ export function useInt() {
 
 export function useIntEx() {
   factories[MemberType.Int] = getIntAccessorEx;
+}
+
+export function useUint() {
+  factories[MemberType.Uint] = getUintAccessor;
+}
+
+export function useUintEx() {
+  factories[MemberType.Uint] = getUintAccessorEx;
 }
 
 export function useFloat() {
@@ -164,6 +175,16 @@ export function getIntAccessor(access, member, options) {
 
 export function getIntAccessorEx(access, member, options) {
   const getDataViewAccessor = addRuntimeCheck(options, getDataViewIntAccessorEx);
+  return getAccessorUsing(access, member, options, getDataViewAccessor)
+}
+
+export function getUintAccessor(access, member, options) {
+  const getDataViewAccessor = addRuntimeCheck(options, getDataViewUintAccessor);
+  return getAccessorUsing(access, member, options, getDataViewAccessor)
+}
+
+export function getUintAccessorEx(access, member, options) {
+  const getDataViewAccessor = addRuntimeCheck(options, getDataViewUintAccessorEx);
   return getAccessorUsing(access, member, options, getDataViewAccessor)
 }
 

node-zigar-addon/src/addon.cc
@@ -477,10 +477,6 @@ static Local<Object> NewMember(Call* call,
   auto is_required = Boolean::New(isolate, m.is_required);
   def->Set(context, String::NewFromUtf8Literal(isolate, "type"), type).Check();
   def->Set(context, String::NewFromUtf8Literal(isolate, "isRequired"), is_required).Check();
-  if (m.type == MemberType::Int) {
-    auto is_signed = Boolean::New(isolate, m.is_signed);
-    def->Set(context, String::NewFromUtf8Literal(isolate, "isSigned"), is_signed).Check();
-  }
   if (m.bit_size != missing) {
     auto bit_size = Uint32::NewFromUnsigned(isolate, m.bit_size);
     def->Set(context, String::NewFromUtf8Literal(isolate, "bitSize"), bit_size).Check();

node-zigar-addon/src/addon.h
@@ -31,6 +31,7 @@ enum class MemberType : uint32_t {
   Void,
   Bool,
   Int,
+  Uint,
   Float,
   Enum,
   Object,

node-zigar-addon/src/addon.js
@@ -1,9 +1,10 @@
-import { MEMORY, SLOTS } from '../../zigar-runtime/src/symbol.js';
+import { SLOTS } from '../../zigar-runtime/src/symbol.js';
 import { invokeThunk } from '../../zigar-runtime/src/method.js';
 import {
   useVoid,
   useBoolEx,
   useIntEx,
+  useUintEx,
   useFloatEx,
   useEnumerationItemEx,
   useObject,
@@ -34,6 +35,7 @@ import {
 useVoid();
 useBoolEx();
 useIntEx();
+useUintEx();
 useFloatEx();
 useEnumerationItemEx();
 useObject();

zigar-compiler/src/code-generator.js
@@ -39,6 +39,9 @@ export function generateCode(structures, params) {
   if (memberFeatures.useIntEx) {
     delete memberFeatures.useInt;
   }
+  if (memberFeatures.useUintEx) {
+    delete memberFeatures.useUint;
+  }
   if (memberFeatures.useEnumerationItemEx) {
     delete memberFeatures.useEnumerationItem;
   }

zigar-compiler/test/code-generator.test.js
@@ -27,7 +27,6 @@ describe('Code generation', function() {
           members: [
             {
               type: MemberType.Int,
-              isSigned: true,
               bitOffset: 0,
               bitSize: 32,
               byteSize: 4,
@@ -62,7 +61,6 @@ describe('Code generation', function() {
           members: [
             {
               type: MemberType.Int,
-              isSigned: true,
               bitOffset: 0,
               bitSize: 31,
               byteSize: 4,
@@ -445,7 +443,6 @@ describe('Code generation', function() {
             {
               type: MemberType.Object,
               name: "panda",
-              isSigned: true,
               bitOffset: 0,
               bitSize: 32,
               byteSize: 4,
@@ -617,7 +614,6 @@ describe('Code generation', function() {
           members: [
             {
               type: MemberType.Int,
-              isSigned: true,
               isConst: false,
               bitOffset: 0,
               bitSize: 32,
@@ -655,7 +651,6 @@ describe('Code generation', function() {
             members: [
               {
                 type: MemberType.Int,
-                isSigned: true,
                 bitOffset: 0,
                 bitSize,
                 byteSize: 8,

zigar-compiler/zig/exporter.zig
@@ -128,6 +128,7 @@ pub const MemberType = enum(u32) {
     Void = 0,
     Bool,
     Int,
+    Uint,
     Float,
     EnumerationItem,
     Object,
@@ -153,7 +154,6 @@ pub const Member = extern struct {
     name: ?[*:0]const u8 = null,
     member_type: MemberType,
     is_required: bool = false,
-    is_signed: bool = false,
     bit_offset: usize = missing,
     bit_size: usize = missing,
     byte_size: usize = missing,
@@ -416,7 +416,7 @@ test "hasPointerArguments" {
 fn getMemberType(comptime T: type) MemberType {
     return switch (@typeInfo(T)) {
         .Bool => .Bool,
-        .Int => .Int,
+        .Int => |int| if (int.signedness == .signed) .Int else .Uint,
         .Float => .Float,
         .Enum => .EnumerationItem,
         .Struct, .Union, .Array, .ErrorUnion, .Optional, .Pointer, .Vector => .Object,
@@ -813,7 +813,6 @@ fn addMembers(host: anytype, structure: Value, comptime T: type) !void {
 fn addPrimitiveMember(host: anytype, structure: Value, comptime T: type) !void {
     try host.attachMember(structure, .{
         .member_type = getMemberType(T),
-        .is_signed = isSigned(T),
         .bit_size = @bitSizeOf(T),
         .bit_offset = 0,
         .byte_size = @sizeOf(T),
@@ -825,7 +824,6 @@ fn addArrayMember(host: anytype, structure: Value, comptime T: type) !void {
     const ar = @typeInfo(T).Array;
     try host.attachMember(structure, .{
         .member_type = getMemberType(ar.child),
-        .is_signed = isSigned(ar.child),
         .bit_size = @bitSizeOf(ar.child),
         .byte_size = @sizeOf(ar.child),
         .structure = try getStructure(host, ar.child),
@@ -836,7 +834,6 @@ fn addVectorMember(host: anytype, structure: Value, comptime T: type) !void {
     const ve = @typeInfo(T).Vector;
     try host.attachMember(structure, .{
         .member_type = getMemberType(ve.child),
-        .is_signed = isSigned(ve.child),
         .bit_size = @bitSizeOf(ve.child),
         .byte_size = @sizeOf(ve.child),
         .structure = try getStructure(host, ve.child),
@@ -861,7 +858,6 @@ fn addPointerMember(host: anytype, structure: Value, comptime T: type) !void {
             try host.writeGlobalSlot(slice_slot, slice_structure);
             try host.attachMember(slice_structure, .{
                 .member_type = getMemberType(pt.child),
-                .is_signed = isSigned(pt.child),
                 .bit_size = @bitSizeOf(pt.child),
                 .byte_size = @sizeOf(pt.child),
                 .structure = child_structure,
@@ -870,7 +866,6 @@ fn addPointerMember(host: anytype, structure: Value, comptime T: type) !void {
                 try host.attachMember(slice_structure, .{
                     .name = "sentinel",
                     .member_type = getMemberType(pt.child),
-                    .is_signed = isSigned(pt.child),
                     .bit_offset = 0,
                     .bit_size = @bitSizeOf(pt.child),
                     .byte_size = @sizeOf(pt.child),
@@ -909,7 +904,6 @@ fn addStructMember(host: anytype, structure: Value, comptime T: type) !void {
             try host.attachMember(structure, .{
                 .name = getCString(field.name),
                 .member_type = getMemberType(field.type),
-                .is_signed = isSigned(field.type),
                 .is_required = field.default_value == null,
                 .bit_offset = @bitOffsetOf(T, field.name),
                 .bit_size = @bitSizeOf(field.type),
@@ -1013,7 +1007,6 @@ fn addUnionMember(host: anytype, structure: Value, comptime T: type) !void {
         try host.attachMember(structure, .{
             .name = getCString(field.name),
             .member_type = getMemberType(field.type),
-            .is_signed = isSigned(field.type),
             .bit_offset = value_offset,
             .bit_size = @bitSizeOf(field.type),
             .byte_size = if (isPacked(T)) missing else @sizeOf(field.type),
@@ -1025,7 +1018,6 @@ fn addUnionMember(host: anytype, structure: Value, comptime T: type) !void {
         try host.attachMember(structure, .{
             .name = "selector",
             .member_type = getMemberType(TT),
-            .is_signed = isSigned(TT),
             .bit_offset = tag_offset,
             .bit_size = @bitSizeOf(TT),
             .byte_size = if (isPacked(T)) missing else @sizeOf(TT),
@@ -1044,7 +1036,6 @@ fn addEnumMember(host: anytype, structure: Value, comptime T: type) !void {
         try host.attachMember(structure, .{
             .name = getCString(field.name),
             .member_type = getMemberType(IT),
-            .is_signed = isSigned(IT),
             .bit_size = @bitSizeOf(IT),
             .byte_size = @sizeOf(IT),
             .structure = try getStructure(host, IT),
@@ -1061,7 +1052,6 @@ fn addOptionalMember(host: anytype, structure: Value, comptime T: type) !void {
     try host.attachMember(structure, .{
         .name = "value",
         .member_type = getMemberType(op.child),
-        .is_signed = isSigned(op.child),
         .bit_offset = 0,
         .bit_size = @bitSizeOf(op.child),
         .byte_size = @sizeOf(op.child),
@@ -1097,7 +1087,6 @@ fn addErrorUnionMember(host: anytype, structure: Value, comptime T: type) !void 
     try host.attachMember(structure, .{
         .name = "value",
         .member_type = getMemberType(eu.payload),
-        .is_signed = isSigned(eu.payload),
         .bit_offset = value_offset,
         .bit_size = @bitSizeOf(eu.payload),
         .byte_size = @sizeOf(eu.payload),
@@ -1106,8 +1095,7 @@ fn addErrorUnionMember(host: anytype, structure: Value, comptime T: type) !void 
     }, false);
     try host.attachMember(structure, .{
         .name = "error",
-        .member_type = .Int,
-        .is_signed = false,
+        .member_type = .Uint,
         .bit_offset = error_offset,
         .bit_size = @bitSizeOf(anyerror),
         .byte_size = @sizeOf(anyerror),

commit 94563b213ab6df172f09bf65980949bb49b369d6

Fixed regression from #192

zigar-compiler/dist/index.cjs
@@ -491,6 +491,12 @@ function getMemberFeature(member) {
       } else {
         return 'useIntEx';
       }
+    case MemberType.Uint:
+      if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
+        return 'useUint';
+      } else {
+        return 'useUintEx';
+      }
     case MemberType.EnumerationItem:
       if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
         return 'useEnumerationItem';

zigar-compiler/dist/index.js
@@ -489,6 +489,12 @@ function getMemberFeature(member) {
       } else {
         return 'useIntEx';
       }
+    case MemberType.Uint:
+      if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
+        return 'useUint';
+      } else {
+        return 'useUintEx';
+      }
     case MemberType.EnumerationItem:
       if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
         return 'useEnumerationItem';

zigar-runtime/dist/index.js
@@ -1934,7 +1934,7 @@ function finalizePointer(s) {
         initializer.call(this, arg[SLOTS][0]);
       } else {
         // not doing memory copying since the value stored there likely isn't valid
-        copyPointer$1.call(this, arg);
+        copyPointer.call(this, arg);
       }
     } else {
       const Target = targetStructure.constructor;
@@ -1942,7 +1942,7 @@ function finalizePointer(s) {
         if (!isConst && arg.constructor.const) {
           throwConstantConstraint(s, arg);
         }
-        copyPointer$1.call(this, arg);
+        copyPointer.call(this, arg);
       } else {
         if (!(arg instanceof Target)) {
           if (isCompatible(arg, Target)) {
@@ -2000,7 +2000,7 @@ function getProxy() {
   return this[PROXY];
 }
 
-function copyPointer$1(src) {
+function copyPointer(src) {
   this[SLOTS][0] = src[SLOTS][0];
 }
 
@@ -2176,7 +2176,7 @@ function finalizeArray(s) {
       restoreMemory.call(arg);
       copy(this[MEMORY], arg[MEMORY]);
       if (hasPointer) {
-        this[POINTER_VISITOR](true, arg, copyPointer$1);
+        this[POINTER_VISITOR](true, arg, copyPointer);
       }
     } else {
       if (typeof(arg) === 'string' && specialKeys.includes('string')) {
@@ -2258,13 +2258,8 @@ function addPointerVisitor$1(s) {
   const { constructor: { prototype } } = s;
   const visitor = function visitPointers(vivificating, src, fn) {
     for (let i = 0, len = this.length; i < len; i++) {
-      let srcChild;
-      if (src) {
-        srcChild = src[SLOTS][i];
-        if (!srcChild) {
-          continue;
-        }
-      }
+      // no need to check for empty slots, since that isn't possible
+      const srcChild = src?.[SLOTS][i];
       const child = (vivificating) ? this[CHILD_VIVIFICATOR](i) : this[SLOTS][i];
       if (child) {
         child[POINTER_VISITOR](vivificating, srcChild, fn);
@@ -2560,7 +2555,7 @@ function finalizeStruct(s) {
       restoreMemory.call(arg);
       copy(this[MEMORY], arg[MEMORY]);
       if (hasPointer) {
-        this[POINTER_VISITOR](true, template, copyPointer$1);
+        this[POINTER_VISITOR](true, template, copyPointer);
       }
     } else {
       if (arg && typeof(arg) === 'object') {
@@ -2587,7 +2582,7 @@ function finalizeStruct(s) {
         if (template && !specialInit && found < members.length) {
           copy(this[MEMORY], template[MEMORY]);
           if (hasPointer) {
-            this[POINTER_VISITOR](true, template, copyPointer$1);
+            this[POINTER_VISITOR](true, template, copyPointer);
           }
         }
         for (const key of keys) {
@@ -2645,6 +2640,8 @@ function addPointerVisitor(s) {
     for (const { slot } of pointerMembers) {
       let srcChild;
       if (src) {
+        // when src is a the struct's template, most slots will likely be empty,
+        // since point fields aren't likely to have default values
         srcChild = src[SLOTS][slot];
         if (!srcChild) {
           continue;
@@ -2705,7 +2702,7 @@ function finalizeUnion(s) {
       };
     }
     for (const member of valueMembers) {
-      const { name, slot } = member;
+      const { name, slot, structure: { hasPointer } } = member;
       const { get: getValue, set: setValue } = getAccessors(member, options);
       const update = (isTagged) ? function(name) {
         if (this[TAG]?.name !== name) {
@@ -2803,7 +2800,7 @@ function finalizeUnion(s) {
       restoreMemory.call(arg);
       copy(this[MEMORY], arg[MEMORY]);
       if (hasPointer) {
-        this[POINTER_VISITOR](true, arg, copyPointer$1);
+        this[POINTER_VISITOR](true, arg, copyPointer);
       }
     } else {
       if (arg && typeof(arg) === 'object') {
@@ -2837,7 +2834,7 @@ function finalizeUnion(s) {
             restoreMemory.call(this);
             copy(this[MEMORY], template[MEMORY]);
             if (hasPointer) {
-              this[POINTER_VISITOR](true, template, copyPointer$1);
+              this[POINTER_VISITOR](true, template, copyPointer);
             }
           }
         } else {
@@ -2912,7 +2909,7 @@ function finalizeErrorUnion(s) {
       copy(this[MEMORY], arg[MEMORY]);
       if (hasPointer) {
         if (check.call(this)) {
-          this[POINTER_VISITOR](true, arg, copyPointer$1);
+          this[POINTER_VISITOR](true, arg, copyPointer);
         }
       }
     } else {
@@ -3299,7 +3296,7 @@ function finalizeSlice(s) {
       restoreMemory.call(arg);
       copy(this[MEMORY], arg[MEMORY]);
       if (hasPointer) {
-        this[POINTER_VISITOR](true, arg, copyPointer$1);
+        this[POINTER_VISITOR](true, arg, copyPointer);
       }
     } else {
       if (typeof(arg) === 'string' && specialKeys.includes('string')) {

zigar-runtime/src/member.js
@@ -95,6 +95,12 @@ export function getMemberFeature(member) {
       } else {
         return 'useIntEx';
       }
+    case MemberType.Uint:
+      if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
+        return 'useUint';
+      } else {
+        return 'useUintEx';
+      }
     case MemberType.EnumerationItem:
       if(isByteAligned(member) && (bitSize === 8 || bitSize === 16 || bitSize === 32 || bitSize === 64)) {
         return 'useEnumerationItem';

rollup-plugin-zigar/test/rollup-project/sha1.js
@@ -1,4 +1,4 @@
-import { usePrimitive, useSlice, usePointer, useArray, useArgStruct, useStruct, useInt, useObject, finalizeStructures, linkModule } from 'zigar-runtime';
+import { usePrimitive, useSlice, usePointer, useArray, useArgStruct, useStruct, useUint, useObject, useInt, finalizeStructures, linkModule } from 'zigar-runtime';
 
 // activate features
 usePrimitive();
@@ -7,16 +7,13 @@ usePointer();
 useArray();
 useArgStruct();
 useStruct();
-useInt();
+useUint();
 useObject();
+useInt();
 
 // define structures
 const s = {
   constructor: null,
-  initializer: null,
-  pointerCopier: null,
-  pointerResetter: null,
-  pointerDisabler: null,
   typedArray: null,
   type: 0,
   name: undefined,
@@ -51,8 +48,7 @@ const s0 = {
     members: [
       {
         ...m,
-        type: 2,
-        isSigned: false,
+        type: 3,
         bitOffset: 0,
         bitSize: 8,
         byteSize: 1,
@@ -74,8 +70,7 @@ const s1 = {
     members: [
       {
         ...m,
-        type: 2,
-        isSigned: false,
+        type: 3,
         bitSize: 8,
         byteSize: 1,
         structure: s0,
@@ -97,7 +92,7 @@ const s2 = {
     members: [
       {
         ...m,
-        type: 5,
+        type: 6,
         bitSize: 64,
         byteSize: 8,
         slot: 0,
@@ -119,8 +114,7 @@ const s3 = {
     members: [
       {
         ...m,
-        type: 2,
-        isSigned: false,
+        type: 3,
         bitSize: 8,
         byteSize: 1,
         structure: s0,
@@ -141,7 +135,7 @@ const s4 = {
     members: [
       {
         ...m,
-        type: 5,
+        type: 6,
         isRequired: true,
         bitOffset: 0,
         bitSize: 64,
@@ -152,7 +146,7 @@ const s4 = {
       },
       {
         ...m,
-        type: 5,
+        type: 6,
         isRequired: true,
         bitOffset: 64,
         bitSize: 320,
@@ -199,7 +193,7 @@ const module = s5.constructor;
 
 // initiate loading and compilation of WASM bytecodes
 const wasmPromise = (async () => {
-  const url = new URL('assets/sha1-e57d84bf.wasm', import.meta.url).href;
+  const url = new URL('assets/sha1-e8371cbb.wasm', import.meta.url).href;
   if (typeof(process) === 'object' && process[Symbol.toStringTag] === 'process') {
     const { readFile } = await import('fs/promises');
     const { fileURLToPath } = await import('url');
@@ -209,7 +203,7 @@ const wasmPromise = (async () => {
     return fetch(url);
   }
 })();
-const __init = linkModule(wasmPromise, linkage);
+const __init = linkModule(wasmPromise, { ...linkage, writeBack: true });
 
 // export functions, types, and constants
 const {