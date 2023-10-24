f67189797670ec59ff032bcf408b89037ccfb2d0
Chung Leong <chernyshevsky@hotmail.com>
2023-10-24 02:46:35 +0200

    Added demo for zigar-loader (#177)

---
+++ zigar-loader/demos/demo-2/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: {
+    path: path.join(path.resolve(`${__dirname}/../../../docs`), '/demo-2'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.zig$/,
+        exclude: /node_modules/,
+        use: '../../dist/index.js',
+      },
+    ],
+  },
+};
