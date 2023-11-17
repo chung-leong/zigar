e6561b274841fcf0a444be4ce8f3ee0f7517c9d9
Chung Leong <chernyshevsky@hotmail.com>
2023-11-17 17:26:57 +0100

    REmoved unnecessary conversion to int (#212)

--- zigar-compiler/zig/wasm-exporter.zig
+++ zigar-compiler/zig/wasm-exporter.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const builtin = @import("builtin");
 const exporter = @import("exporter.zig");
 
-const Value = exporter.Value;
+pub const Value = exporter.Value;
+pub const Thunk = exporter.Thunk;
+pub const Call = *const CallContext;
+
 const StructureType = exporter.StructureType;
 const Structure = exporter.Structure;
 const MemberType = exporter.MemberType;
@@ -12,46 +15,36 @@ const Memory = exporter.Memory;
 const MemoryDisposition = exporter.MemoryDisposition;
 const MemoryAttributes = exporter.MemoryAttributes;
 const Template = exporter.Template;
-const Thunk = exporter.Thunk;
 const Error = exporter.Error;
 const missing = exporter.missing;
 
 const CallContext = struct {
     allocator: std.mem.Allocator,
 };
-const Call = *const CallContext;
 
 extern fn _allocateRelocatableMemory(len: usize, alignment: u16) usize;
-extern fn _freeRelocatableMemory(address: usize, len: usize, alignment: u16) void;
-extern fn _createString(address: usize, len: usize) usize;
-extern fn _createObject(structure: usize, dv: usize) usize;
-extern fn _createView(address: usize, len: usize, copy: bool) usize;
-extern fn _castView(structure: usize, dv: usize) usize;
-extern fn _readSlot(container: usize, slot: usize) usize;
-extern fn _getViewAddress(dv: usize) usize;
-extern fn _writeSlot(container: usize, slot: usize, object: usize) void;
-extern fn _beginDefinition() usize;
-extern fn _insertInteger(container: usize, key: usize, value: i32) void;
-extern fn _insertBoolean(container: usize, key: usize, value: i32) void;
-extern fn _insertString(container: usize, key: usize, value: usize) void;
-extern fn _insertObject(container: usize, key: usize, value: usize) void;
-extern fn _beginStructure(def: u32) u32;
-extern fn _attachMember(structure: usize, def: usize, is_static: i32) void;
-extern fn _attachMethod(structure: usize, def: usize, is_static_only: i32) void;
-extern fn _attachTemplate(structure: usize, def: usize, is_static: i32) void;
-extern fn _finalizeStructure(structure: usize) void;
-extern fn _createTemplate(buffer: usize) usize;
-extern fn _writeToConsole(dv: usize) i32;
-extern fn _startCall(call_addr: usize, arg_struct: usize) usize;
-extern fn _endCall(call_addr: usize, arg_struct: usize) void;
-
-fn ref(number: usize) Value {
-    return @ptrFromInt(number);
-}
-
-fn index(object: ?Value) usize {
-    return if (object) |ptr| @intFromPtr(ptr) else 0;
-}
+extern fn _freeRelocatableMemory(bytes: [*]u8, len: usize, alignment: u16) void;
+extern fn _createString(bytes: ?[*]const u8, len: usize) ?Value;
+extern fn _createObject(structure: Value, dv: Value) ?Value;
+extern fn _createView(bytes: ?[*]u8, len: usize, copy: bool) ?Value;
+extern fn _castView(structure: Value, dv: Value) ?Value;
+extern fn _getViewAddress(dv: Value) usize;
+extern fn _readSlot(container: ?Value, slot: usize) ?Value;
+extern fn _writeSlot(container: ?Value, slot: usize, object: ?Value) void;
+extern fn _beginDefinition() Value;
+extern fn _insertInteger(container: Value, key: Value, value: i32) void;
+extern fn _insertBoolean(container: Value, key: Value, value: bool) void;
+extern fn _insertString(container: Value, key: Value, value: Value) void;
+extern fn _insertObject(container: Value, key: Value, value: ?Value) void;
+extern fn _beginStructure(def: Value) ?Value;
+extern fn _attachMember(structure: Value, def: Value, is_static: bool) void;
+extern fn _attachMethod(structure: Value, def: Value, is_static_only: bool) void;
+extern fn _attachTemplate(structure: Value, def: Value, is_static: bool) void;
+extern fn _finalizeStructure(structure: Value) void;
+extern fn _createTemplate(buffer: ?Value) ?Value;
+extern fn _writeToConsole(dv: Value) bool;
+extern fn _startCall(call: Call, arg_struct: ?Value) *anyopaque;
+extern fn _endCall(call: Call, arg_struct: ?Value) void;
 
 fn strlen(s: [*:0]const u8) usize {
     var len: usize = 0;
@@ -70,36 +63,32 @@ pub fn getPtrAlign(alignment: u16) u8 {
     return if (alignment != 0) std.math.log2_int(u16, alignment) else 0;
 }
 
-pub fn allocateFixedMemory(len: usize, alignment: u16) usize {
+pub fn allocateFixedMemory(len: usize, alignment: u16) ?Value {
     const ptr_align = getPtrAlign(alignment);
     if (allocator.rawAlloc(len, ptr_align, 0)) |bytes| {
-        return _createView(@intFromPtr(bytes), len, false);
+        return _createView(bytes, len, false);
     } else {
-        return 0;
+        return null;
     }
 }
 
-pub fn freeFixedMemory(bytes_addr: usize, len: usize, alignment: u16) void {
-    const bytes: [*]u8 = @ptrFromInt(bytes_addr);
+pub fn freeFixedMemory(bytes: [*]u8, len: usize, alignment: u16) void {
     const ptr_align = getPtrAlign(alignment);
     allocator.rawFree(bytes[0..len], ptr_align, 0);
 }
 
-pub fn allocateShadowMemory(call_addr: usize, len: usize, alignment: u16) usize {
-    const ctx: Call = @ptrFromInt(call_addr);
+pub fn allocateShadowMemory(call: Call, len: usize, alignment: u16) ?Value {
     const ptr_align = getPtrAlign(alignment);
-    if (ctx.allocator.rawAlloc(len, ptr_align, 0)) |bytes| {
-        return _createView(@intFromPtr(bytes), len, false);
+    if (call.allocator.rawAlloc(len, ptr_align, 0)) |bytes| {
+        return _createView(bytes, len, false);
     } else {
-        return 0;
+        return null;
     }
 }
 
-pub fn freeShadowMemory(call_addr: usize, bytes_addr: usize, len: usize, alignment: u16) void {
-    const ctx: Call = @ptrFromInt(call_addr);
-    const bytes: [*]u8 = @ptrFromInt(bytes_addr);
+pub fn freeShadowMemory(call: Call, bytes: [*]u8, len: usize, alignment: u16) void {
     const ptr_align = getPtrAlign(alignment);
-    ctx.allocator.rawFree(bytes[0..len], ptr_align, 0);
+    call.allocator.rawFree(bytes[0..len], ptr_align, 0);
 }
 
 pub const Host = struct {
@@ -121,105 +110,104 @@ pub const Host = struct {
     }
 
     pub fn allocateMemory(_: Host, size: usize, alignment: u16) !Memory {
-        const dv_index = _allocateRelocatableMemory(size, alignment);
-        if (dv_index == 0) {
+        if (_allocateRelocatableMemory(size, alignment)) |dv| {
+            const address = _getViewAddress(dv);
+            return .{
+                .bytes = @ptrFromInt(address),
+                .len = size,
+            };
+        } else {
             return Error.UnableToAllocateMemory;
         }
-        const address = _getViewAddress(dv_index);
-        return .{
-            .bytes = @ptrFromInt(address),
-            .len = size,
-        };
     }
 
     pub fn freeMemory(_: Host, memory: Memory) !void {
-        _freeRelocatableMemory(@intFromPtr(memory.bytes), memory.len, memory.attributes.alignment);
+        if (memory.bytes) |bytes| {
+            _freeRelocatableMemory(bytes, memory.len, memory.attributes.alignment);
+        }
     }
 
     pub fn createString(_: Host, memory: Memory) !Value {
-        const bytes = memory.bytes;
-        const len = memory.len;
-        const address = @intFromPtr(bytes);
-        const str_index = _createString(address, len);
-        if (str_index == 0) {
+        if (_createString(memory.bytes, memory.len)) |str| {
+            return str;
+        } else {
             return Error.UnableToCreateString;
         }
-        return ref(str_index);
     }
 
     pub fn createObject(_: Host, structure: Value, arg: Value) !Value {
-        const value_index = _createObject(index(structure), index(arg));
-        if (value_index == 0) {
+        if (_createObject(structure, arg)) |object| {
+            return object;
+        } else {
             return Error.UnableToCreateObject;
         }
-        return ref(value_index);
     }
 
     pub fn createView(_: Host, memory: Memory) !Value {
-        const bytes = memory.bytes;
-        const len = memory.len;
-        const address = @intFromPtr(bytes);
-        const copy = memory.attributes.is_comptime;
-        const dv_index = _createView(address, len, copy);
-        if (dv_index == 0) {
+        if (_createView(memory.bytes, memory.len, memory.attributes.is_comptime)) |dv| {
+            return dv;
+        } else {
             return Error.UnableToCreateDataView;
         }
-        return ref(dv_index);
     }
 
     pub fn castView(_: Host, structure: Value, dv: Value) !Value {
-        const value_index = _castView(index(structure), index(dv));
-        if (value_index == 0) {
+        if (_castView(structure, dv)) |object| {
+            return object;
+        } else {
             return Error.UnableToCreateObject;
         }
-        return ref(value_index);
     }
 
     pub fn readSlot(_: Host, container: ?Value, slot: usize) !Value {
-        const obj_index = _readSlot(index(container), slot);
-        if (obj_index == 0) {
+        if (_readSlot(container, slot)) |value| {
+            return value;
+        } else {
             return Error.UnableToRetrieveObject;
         }
-        return ref(obj_index);
     }
 
     pub fn writeSlot(_: Host, container: ?Value, slot: usize, value: ?Value) !void {
-        _writeSlot(index(container), slot, index(value));
+        _writeSlot(container, slot, value);
     }
 
     fn beginDefinition() Value {
-        return ref(_beginDefinition());
+        return _beginDefinition();
     }
 
-    fn insertProperty(container: Value, key: []const u8, value: anytype) void {
+    fn insertProperty(container: Value, key: []const u8, value: anytype) !void {
         const T = @TypeOf(value);
-        const key_index = _createString(@intFromPtr(key.ptr), key.len);
+        switch (@typeInfo(T)) {
+            .Optional => {
+                if (value) |v| {
+                    return insertProperty(container, key, v);
+                }
+            },
+            else => {},
+        }
+        const key_str = _createString(key.ptr, key.len) orelse return Error.UnableToCreateString;
         switch (@typeInfo(T)) {
             .Pointer => {
                 if (T == [*:0]const u8) {
-                    const value_index = _createString(@intFromPtr(value), strlen(value));
-                    _insertString(index(container), key_index, value_index);
+                    const str = _createString(value, strlen(value)) orelse return Error.UnableToCreateString;
+                    _insertString(container, key_str, str);
                 } else if (T == Value) {
-                    _insertObject(index(container), key_index, index(value));
+                    _insertObject(container, key_str, value);
                 } else {
                     @compileError("No support for value type: " ++ @typeName(T));
                 }
             },
             .Int => {
-                _insertInteger(index(container), key_index, @intCast(value));
+                _insertInteger(container, key_str, @intCast(value));
             },
             .Enum => {
-                _insertInteger(index(container), key_index, @intCast(@intFromEnum(value)));
+                _insertInteger(container, key_str, @intCast(@intFromEnum(value)));
             },
             .Bool => {
-                _insertBoolean(index(container), key_index, if (value) 1 else 0);
+                _insertBoolean(container, key_str, value);
             },
             .Optional => {
-                if (value) |v| {
-                    insertProperty(container, key, v);
-                } else {
-                    _insertObject(index(container), key_index, 0);
-                }
+                _insertObject(container, key_str, null);
             },
             else => {
                 @compileError("No support for value type: " ++ @typeName(T));
@@ -229,70 +217,73 @@ pub const Host = struct {
 
     pub fn beginStructure(_: Host, def: Structure) !Value {
         const structure = beginDefinition();
-        insertProperty(structure, "name", def.name);
-        insertProperty(structure, "type", def.structure_type);
-        insertProperty(structure, "length", def.length);
-        insertProperty(structure, "byteSize", def.byte_size);
-        insertProperty(structure, "align", def.alignment);
-        insertProperty(structure, "isConst", def.is_const);
-        insertProperty(structure, "hasPointer", def.has_pointer);
-        return ref(_beginStructure(index(structure)));
+        try insertProperty(structure, "name", def.name);
+        try insertProperty(structure, "type", def.structure_type);
+        try insertProperty(structure, "length", def.length);
+        try insertProperty(structure, "byteSize", def.byte_size);
+        try insertProperty(structure, "align", def.alignment);
+        try insertProperty(structure, "isConst", def.is_const);
+        try insertProperty(structure, "hasPointer", def.has_pointer);
+        if (_beginStructure(structure)) |s| {
+            return s;
+        } else {
+            return Error.UnableToStartStructureDefinition;
+        }
     }
 
     pub fn attachMember(_: Host, structure: Value, member: Member, is_static: bool) !void {
         const def = beginDefinition();
-        insertProperty(def, "type", member.member_type);
-        insertProperty(def, "isRequired", member.is_required);
+        try insertProperty(def, "type", member.member_type);
+        try insertProperty(def, "isRequired", member.is_required);
         if (member.bit_offset != missing) {
-            insertProperty(def, "bitOffset", member.bit_offset);
+            try insertProperty(def, "bitOffset", member.bit_offset);
         }
         if (member.bit_size != missing) {
-            insertProperty(def, "bitSize", member.bit_size);
+            try insertProperty(def, "bitSize", member.bit_size);
         }
         if (member.byte_size != missing) {
-            insertProperty(def, "byteSize", member.byte_size);
+            try insertProperty(def, "byteSize", member.byte_size);
         }
         if (member.slot != missing) {
-            insertProperty(def, "slot", member.slot);
+            try insertProperty(def, "slot", member.slot);
         }
         if (member.name) |name| {
-            insertProperty(def, "name", name);
+            try insertProperty(def, "name", name);
         }
         if (member.structure) |s| {
-            insertProperty(def, "structure", s);
+            try insertProperty(def, "structure", s);
         }
-        _attachMember(index(structure), index(def), if (is_static) 1 else 0);
+        _attachMember(structure, def, is_static);
     }
 
     pub fn attachMethod(_: Host, structure: Value, method: Method, is_static_only: bool) !void {
         const def = beginDefinition();
-        insertProperty(def, "argStruct", method.structure);
-        insertProperty(def, "thunk", @intFromPtr(method.thunk));
+        try insertProperty(def, "argStruct", method.structure);
+        try insertProperty(def, "thunk", @intFromPtr(method.thunk));
         if (method.name) |name| {
-            insertProperty(def, "name", name);
+            try insertProperty(def, "name", name);
         }
-        _attachMethod(index(structure), index(def), if (is_static_only) 1 else 0);
+        _attachMethod(structure, def, is_static_only);
     }
 
     pub fn attachTemplate(_: Host, structure: Value, template: Value, is_static: bool) !void {
-        _attachTemplate(index(structure), index(template), if (is_static) 1 else 0);
+        _attachTemplate(structure, template, is_static);
     }
 
     pub fn finalizeStructure(_: Host, structure: Value) !void {
-        _finalizeStructure(index(structure));
+        _finalizeStructure(structure);
     }
 
     pub fn createTemplate(_: Host, dv: ?Value) !Value {
-        const templ_index = _createTemplate(index(dv));
-        if (templ_index == 0) {
+        if (_createTemplate(dv)) |templ| {
+            return templ;
+        } else {
             return Error.UnableToCreateStructureTemplate;
         }
-        return ref(templ_index);
     }
 
     pub fn writeToConsole(_: Host, dv: Value) !void {
-        const result = _writeToConsole(index(dv));
-        if (result == 0) {
+        if (!_writeToConsole(dv)) {
             return Error.UnableToWriteToConsole;
         }
     }
@@ -312,27 +303,27 @@ pub const Host = struct {
     }
 };
 
-pub fn runThunk(fn_address: usize, arg_struct: usize) usize {
+pub fn runThunk(thunk: Thunk, arg_struct: ?Value) ?Value {
     // note that std.debug.print() doesn't work here since the initial context is not set
-    const thunk: Thunk = @ptrFromInt(fn_address);
-    var ctx: CallContext = .{
+    var call_ctx: CallContext = .{
         .allocator = .{ .ptr = undefined, .vtable = &std.heap.WasmAllocator.vtable },
     };
-    const ptr: *anyopaque = @ptrCast(&ctx);
-    const arg_address = _startCall(@intFromPtr(ptr), arg_struct);
-    defer _endCall(@intFromPtr(ptr), arg_struct);
-    const arg_ptr: *anyopaque = @ptrFromInt(arg_address);
-    if (thunk(ptr, arg_ptr)) |result| {
-        return index(result);
+    const arg_ptr = _startCall(&call_ctx, arg_struct);
+    defer _endCall(&call_ctx, arg_struct);
+    if (thunk(@ptrCast(&call_ctx), arg_ptr)) |result| {
+        return result;
     } else {
-        return 0;
+        return null;
     }
 }
 
-pub fn defineStructures(comptime T: type) usize {
+pub fn defineStructures(comptime T: type) ?Value {
     const factory = exporter.createRootFactory(Host, T);
-    const fn_address: usize = @intFromPtr(factory);
-    return runThunk(fn_address, 0);
+    return runThunk(factory, null);
+}
+
+pub fn isRuntimeSafetyActive() bool {
+    return builtin.mode == .ReleaseSafe or builtin.mode == .Debug;
 }
 
 pub fn getOS() type {
@@ -358,8 +349,4 @@ pub fn getOS() type {
             }
         };
     };
-}
-
-pub fn isRuntimeSafetyActive() u8 {
-    return if (builtin.mode == .ReleaseSafe or builtin.mode == .Debug) 1 else 0;
 }
--- zigar-compiler/zig/wasm-stub.zig
+++ zigar-compiler/zig/wasm-stub.zig
@@ -2,32 +2,36 @@ const std = @import("std");
 const exporter = @import("exporter");
 const package = @import("package");
 
+const Value = exporter.Value;
+const Thunk = exporter.Thunk;
+const Call = exporter.Call;
+
 pub const os = exporter.getOS();
 
-export fn allocateFixedMemory(len: usize, alignment: u16) usize {
+export fn allocateFixedMemory(len: usize, alignment: u16) ?Value {
     return exporter.allocateFixedMemory(len, alignment);
 }
 
-export fn freeFixedMemory(byte_addr: usize, len: usize, alignment: u16) void {
-    exporter.freeFixedMemory(byte_addr, len, alignment);
+export fn freeFixedMemory(bytes: [*]u8, len: usize, alignment: u16) void {
+    exporter.freeFixedMemory(bytes, len, alignment);
 }
 
-export fn allocateShadowMemory(call_addr: usize, len: usize, alignment: u16) usize {
-    return exporter.allocateShadowMemory(call_addr, len, alignment);
+export fn allocateShadowMemory(call: Call, len: usize, alignment: u16) ?Value {
+    return exporter.allocateShadowMemory(call, len, alignment);
 }
 
-export fn freeShadowMemory(call_addr: usize, byte_addr: usize, len: usize, alignment: u16) void {
-    exporter.freeShadowMemory(call_addr, byte_addr, len, alignment);
+export fn freeShadowMemory(call: Call, bytes: [*]u8, len: usize, alignment: u16) void {
+    exporter.freeShadowMemory(call, bytes, len, alignment);
 }
 
-export fn defineStructures() usize {
+export fn defineStructures() ?Value {
     return exporter.defineStructures(package);
 }
 
-export fn runThunk(thunk_address: usize, arg_struct: usize) usize {
-    return exporter.runThunk(thunk_address, arg_struct);
+export fn runThunk(thunk: Thunk, arg_struct: ?Value) ?Value {
+    return exporter.runThunk(thunk, arg_struct);
 }
 
-export fn isRuntimeSafetyActive() u8 {
+export fn isRuntimeSafetyActive() bool {
     return exporter.isRuntimeSafetyActive();
 }
