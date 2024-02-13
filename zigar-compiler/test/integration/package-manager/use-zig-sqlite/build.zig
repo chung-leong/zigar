32be1bc43c2b61b234ecfb0a737bdcbbe1aa4c0c
Chung Leong <chernyshevsky@hotmail.com>
2024-02-13 15:40:43 +0100

    Updated test build files

--- zigar-compiler/test/integration/package-manager/use-zig-sqlite/build.zig
+++ zigar-compiler/test/integration/package-manager/use-zig-sqlite/build.zig
@@ -2,16 +2,22 @@ const std = @import("std");
 const cfg = @import("./build-cfg.zig");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{
-        .default_target = cfg.target,
-    });
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const arch = if (@hasDecl(@TypeOf(target), "getCpuArch")) target.getCpuArch() else target.result.cpu.arch;
+    const is_wasm = switch (arch) {
+        .wasm32, .wasm64 => true,
+        else => false,
+    };
     const lib = b.addSharedLibrary(.{
         .name = cfg.package_name,
         .root_source_file = .{ .path = cfg.stub_path },
         .target = target,
         .optimize = optimize,
     });
+    if (is_wasm) {
+        lib.rdynamic = true;
+    }
     const sqlite = b.dependency("sqlite", .{
         .target = target,
         .optimize = optimize,
@@ -37,12 +43,18 @@ pub fn build(b: *std.Build) void {
             .root_source_file = .{ .path = cfg.package_path },
             .imports = &imports,
         }));
+        if (is_wasm) {
+            // WASM needs to be compiled as exe
+            lib.kind = .exe;
+            lib.linkage = .static;
+            lib.entry = .disabled;
+        }
     }
-    lib.linkLibC();
-    lib.linkLibrary(sqlite.artifact("sqlite"));
-    if (cfg.target.cpu_arch == .wasm32) {
-        lib.use_lld = false;
-        lib.rdynamic = true;
+    if (cfg.use_libc) {
+        lib.linkLibC();
     }
-    b.installArtifact(lib);
+    const wf = b.addWriteFiles();
+    wf.addCopyFileToSource(lib.getEmittedBin(), cfg.output_path);
+    wf.step.dependOn(&lib.step);
+    b.getInstallStep().dependOn(&wf.step);
 }
