f8b4a13c79514e19af9bfc5d53407e384b5b092f
Chung Leong <chernyshevsky@hotmail.com>
2024-03-01 01:46:10 +0100

    Updated all package.json files

--- node-zigar/test/index-cjs.test.js
+++ node-zigar/test/index-cjs.test.js
@@ -1,8 +1,10 @@
-const { expect } = require('chai');
-require('../dist/index.cjs');
+import { expect } from 'chai';
+import { createRequire } from 'module';
+import '../dist/index.cjs';
 
 describe('CommonJS loader', function() {
   describe('require', function() {
+    const require = createRequire(import.meta.url);
     it('should load Zig file', async function() {
       this.timeout(60000);
       const uri = './zig-samples/function-simple.zig';
@@ -10,8 +12,8 @@ describe('CommonJS loader', function() {
       expect(module.add).to.be.a('function');
     })
     it('should ignore URL without the extension zig', async function() {
-      const module = require('chai');
-      expect(module.expect).to.equal(expect);
+      const module = require('module');
+      expect(module.createRequire).to.equal(createRequire);
     })
     it('should use query variables', async function() {
       this.timeout(300000);
