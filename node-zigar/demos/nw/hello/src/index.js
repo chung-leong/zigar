9f0339b265c637a5458f5e56cd6e85e747c2738a
Chung Leong <chernyshevsky@hotmail.com>
2024-03-22 15:34:16 +0100

    Added example for nw.js (#297)

---
+++ node-zigar/demos/nw/hello/src/index.js
@@ -0,0 +1,6 @@
+require('node-zigar/cjs');
+const { hello } = require('./lib/hello.zigar');
+console.log = (s) => process.stderr.write(`${s}\n`);
+hello(); 
+
+nw.Window.open('./src/index.html', { width: 800, height: 600 });
