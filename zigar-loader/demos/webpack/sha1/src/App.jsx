d1f164defd749aeade37540d960a22a16f793362
Chung Leong <chernyshevsky@hotmail.com>
2024-03-24 00:29:17 +0100

    Added sha1 example for Webpack (#298)

---
+++ zigar-loader/demos/webpack/sha1/src/App.jsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from 'react';
+import { sha1 } from '../zig/sha1.zig';
+
+function App() {
+  const [ digest, setDigest ] = useState('-');
+  const onChange = useCallback(async (evt) => {
+    const [ file ] = evt.target.files;
+    if (file) {
+      const buffer = await file.arrayBuffer();
+      const { string } = sha1(buffer);
+      setDigest(string); 
+    } else {
+      setDigest('-'); 
+    }
+  });
+  return (
+    <div className="App">
+      <input type="file" onChange={onChange} />
+      <h2>{digest}</h2>
+    </div>
+  );
+}
+
+export default App
