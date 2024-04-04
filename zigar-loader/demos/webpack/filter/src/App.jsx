98b1b62d28f0cdce8e7e1850a857c21ddf9bcef4
Chung Leong <chernyshevsky@hotmail.com>
2024-04-05 00:08:51 +0200

    Began filter example for Webpack (#299)

---
+++ zigar-loader/demos/webpack/filter/src/App.jsx
@@ -0,0 +1,93 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import SampleImage from '../img/sample.png';
+import { createOutput } from '../zig/sepia.zig';
+import './App.css';
+
+function App() {
+  const srcCanvasRef = useRef();
+  const dstCanvasRef = useRef();
+  const fileInputRef = useRef();
+  const [ bitmap, setBitmap ] = useState();
+  const [ intensity, setIntensity ] = useState(0.3);
+
+  const onOpenClick = useCallback(() => {
+    fileInputRef.current.click();
+  }, []);
+  const onFileChange = useCallback(async (evt) => {
+    const [ file ] = evt.target.files;
+    if (file) {
+      const bitmap = await createImageBitmap(file);
+      setBitmap(bitmap);
+    }    
+  }, []);
+  const onRangeChange = useCallback((evt) => {
+    setIntensity(evt.target.value);
+  }, [])
+  useEffect(() => {
+    // load initial sample image
+    (async () => {
+      const img = new Image();
+      img.src = SampleImage;
+      await img.decode();
+      const bitmap = await createImageBitmap(img);
+      setBitmap(bitmap);
+    })();
+  }, []);
+  useEffect(() => {
+    // update bitmap after user has selected a different one
+    if (bitmap) {
+      const srcCanvas = srcCanvasRef.current;
+      srcCanvas.width = bitmap.width;
+      srcCanvas.height = bitmap.height;
+      const ctx = srcCanvas.getContext('2d', { willReadFrequently: true });
+      ctx.drawImage(bitmap, 0, 0);
+    }
+  }, [ bitmap ]);
+  useEffect(() => {
+    // update the result when the bitmap or intensity parameter changes
+    (async() => {
+      if (bitmap) {      
+        const srcCanvas = srcCanvasRef.current;
+        const dstCanvas = dstCanvasRef.current;
+        const srcCTX = srcCanvas.getContext('2d', { willReadFrequently: true });
+        const { width, height } = srcCanvas;
+        const srcImageData = srcCTX.getImageData(0, 0, width, height);
+        const dstImageData = await createImageData(width, height, srcImageData, { intensity });
+        dstCanvas.width = width;
+        dstCanvas.height = height;
+        const dstCTX = dstCanvas.getContext('2d');
+        dstCTX.putImageData(dstImageData, 0, 0);
+      }  
+    })();
+  }, [ bitmap, intensity ]);
+
+  return (
+    <div className="App">
+      <div className="nav">
+        <span className="button" onClick={onOpenClick}>Open</span>
+        <input ref={fileInputRef} type="file" className="hidden" accept="image/*" onChange={onFileChange}/>
+      </div>
+      <div className="contents">
+        <div className="pane align-right">
+          <canvas ref={srcCanvasRef}></canvas>
+        </div>
+        <div className="pane align-left">
+          <canvas ref={dstCanvasRef}></canvas>
+          <div className="controls">
+            Intensity: <input type="range" min={0} max={1} step={0.0001} value={intensity} onChange={onRangeChange}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+async function createImageData(width, height, source, params) {
+  const input = { src: source };
+  const output = await createOutput(width, height, input, params);
+  const ta = output.dst.data.typedArray;
+  const clampedArray = new Uint8ClampedArray(ta.buffer, ta.byteOffset, ta.byteLength);
+  return new ImageData(clampedArray, width, height);
+}
---
+++ zigar-loader/demos/webpack/filter/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3030,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.zig$/,
+        exclude: /node_modules/,
+        use: 'zigar-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: 'file-loader',
+      },
+    ],
+  },
+};
