034ed61b2b1679d28d26cb53343433e291931760
Chung Leong <chernyshevsky@hotmail.com>
2024-04-12 15:26:43 +0200

    Added another pointer example (#277)

--- docs/examples/opaque-example-2.js
+++ docs/examples/opaque-example-2.js
@@ -1,6 +1,5 @@
 import { showContext, startContext } from './opaque-example-2.zig';
 
 const ctx = startContext();
-// force garbage collection; require --expose-gc
-gc();
+gc(); // force immediate garbage collection; requires --expose-gc
 showContext(ctx);
---
+++ docs/examples/pointer-example-2.zig
@@ -0,0 +1,2 @@
+pub var int: i32 = 123;
+pub var int_ptr = &int;
