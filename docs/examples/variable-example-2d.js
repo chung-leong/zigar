b4823346f963b36d0c40c18001a993810808b53b
Chung Leong <chernyshevsky@hotmail.com>
2024-04-15 17:02:29 +0200

    Fixed example

--- docs/examples/variable-example-2d.js
+++ docs/examples/variable-example-2d.js
@@ -1,6 +1,7 @@
 import module, { printCurrentUser } from './variable-example-2.zig';
 
 printCurrentUser();
+module.current_user.name.delete();
 module.current_user.delete();
 module.current_user = { name: 'joker1999', role: 'clown' };
 printCurrentUser();
