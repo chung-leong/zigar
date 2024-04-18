303cf7d4922a9788e2cbf7183904426c8a3781ba
Chung Leong <chernyshevsky@hotmail.com>
2024-04-19 01:42:18 +0200

    Updated examples

--- docs/examples/data-transfer-example-1a.js
+++ docs/examples/data-transfer-example-1a.js
@@ -1,7 +1,7 @@
 import { set } from './data-transfer-example-1.zig';
 
 const buffer = new ArrayBuffer(16);
-const i32 = new DataView(buffer, 1, 4);
-const i16 = new DataView(buffer, 3, 2);
-set(i16, i32);
+const int32 = new DataView(buffer, 1, 4);
+const int16 = new DataView(buffer, 3, 2);
+set(int16, int32);
 console.log(buffer);
--- docs/examples/data-transfer-example-1b.js
+++ docs/examples/data-transfer-example-1b.js
@@ -1,10 +1,10 @@
 import { set } from './data-transfer-example-1.zig';
 
 const buffer = new ArrayBuffer(16);
-const i32 = new DataView(buffer, 1, 4);
-const i16 = new DataView(buffer, 4, 2);
+const int32 = new DataView(buffer, 1, 4);
+const int16 = new DataView(buffer, 4, 2);
 try {
-    set(i16, i32);
+    set(int16, int32);
 } catch (err) {
     console.log(err.message);
 }
