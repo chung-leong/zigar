e1f8b40a2733626348f3296ffa60022900bf5adc
Chung Leong <chernyshevsky@hotmail.com>
2024-04-22 14:28:00 +0200

    Refactored code in NW.js SHA1 example

--- node-zigar/demos/nw/sha1/src/index.js
+++ node-zigar/demos/nw/sha1/src/index.js
@@ -1,36 +1,37 @@
-const { readFile } = require('fs/promises');
-const { join } = require('path');
 require('node-zigar/cjs');
 const { sha1 } = require('./lib/sha1.zigar');
 
 const isMac = process.platform === 'darwin'
 
 nw.Window.open('./src/index.html', { width: 800, height: 600 }, (browser) => {
-  // handle menu click
+  // create menu bar
   const onOpenClick = () => {
     const { window: { document } } = browser;
-    const file = document.getElementById('file'); 
-    const heading = document.getElementById('heading');
-    file.onchange = async (evt) => {
-      const { target: { files } } = evt;
-      if (files.length > 0) {
-        const [ file ] = files;
-        const data = await readFile(file.path);
-        const hash = sha1(data).string;
-        heading.textContent = hash;
-      }
-    };
-    file.click();
+    document.getElementById('file').click();
   };
   const onCloseClick = () => {
     browser.close();
   };
-  // create menu bar
   const menuBar = new nw.Menu({ type: 'menubar' });
   const fileMenu = new nw.Menu();
   fileMenu.append(new nw.MenuItem({ label: 'Open', click: onOpenClick }));
   fileMenu.append(new nw.MenuItem({ type: 'separator' }));
   fileMenu.append(new nw.MenuItem({ label: (isMac) ? 'Close' : 'Quit', click: onCloseClick }));
   menuBar.append(new nw.MenuItem({ label: 'File', submenu: fileMenu }));
-  browser.menu = menuBar;    
+  browser.menu = menuBar;
+
+  browser.window.onload = (evt) => {
+    // find page elements and attach handler
+    const { target: document } = evt;
+    const file = document.getElementById('file'); 
+    const heading = document.getElementById('heading');
+    file.onchange = async (evt) => {
+      const { target: { files: [ file ] } } = evt;
+      if (file) {
+        const data = await file.arrayBuffer();
+        const hash = sha1(data).string;
+        heading.textContent = hash;
+      }
+    };
+  };
 });
