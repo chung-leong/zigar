054e66911ef5b9592b02f5ab4148125498f4844c
Chung Leong <chernyshevsky@hotmail.com>
2024-05-07 19:10:44 +0200

    Finished cowsay example

--- rollup-plugin-zigar/demos/vite/cowsay/src/App.jsx
+++ rollup-plugin-zigar/demos/vite/cowsay/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react';
-import { cowsay } from '../zig/cowsay.zig';
+import { useCallback, useState } from 'react';
+import { __zigar, cowsay } from '../zig/cowsay.zig';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
+  const [output, setOutput] = useState('');
+
+  const onChange = useCallback((e) => {
+    const { value } = e.target;
+    setText(value);
+    const args = (value) ? value.split(/[ \t]+/) : [];
+    const lines = [];
+    __zigar.connect({ log: line => lines.push(line) });
+    cowsay([ 'cowsay', ...args ]);
+    setOutput(lines.join('\n'));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Cow</h1>
       <div className="card">
-        <button onClick={() => cowsay([ '', 'Hello' ])}>
-          Cowsay
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input className="input" value={text} onChange={onChange}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <textarea className="output" value={output} readOnly />
+      </div>
     </>
   )
 }
