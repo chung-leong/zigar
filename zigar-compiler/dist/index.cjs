3d05c94cd64724b8f02daf44e998acc41f73fc10
Chung Leong <chernyshevsky@hotmail.com>
2024-05-07 11:59:51 +0200

    Added placeholders for all WASI handlers (#365)

--- zigar-compiler/dist/index.cjs
+++ zigar-compiler/dist/index.cjs
@@ -6740,7 +6740,27 @@ class WebAssemblyEnvironment extends Environment {
     if (this.customWASI) {
       return this.customWASI.wasiImport;
     } else {
+      const ENOSYS = 38;
+      const noImpl = () => ENOSYS;
       return { 
+        args_get: noImpl,
+        args_sizes_get: noImpl,
+        clock_res_get: noImpl,
+        clock_time_get: noImpl,
+        environ_get: noImpl,
+        environ_sizes_get: noImpl,
+        fd_advise: noImpl,
+        fd_allocate: noImpl,
+        fd_close: noImpl,
+        fd_datasync: noImpl,
+        fd_pread: noImpl,
+        fd_pwrite: noImpl,
+        fd_read: noImpl,
+        fd_readdir: noImpl,
+        fd_renumber: noImpl,
+        fd_seek: noImpl,
+        fd_sync: noImpl,
+        fd_tell: noImpl,
         fd_write: (fd, iovs_ptr, iovs_count, written_ptr) => {
           if (fd === 1 || fd === 2) {
             const dv = new DataView(this.memory.buffer);
@@ -6755,9 +6775,29 @@ class WebAssemblyEnvironment extends Environment {
             dv.setUint32(written_ptr, written, true);
             return 0;            
           } else {
-            return 1;
+            return ENOSYS;
           }
         },
+        fd_fdstat_get: noImpl,
+        fd_fdstat_set_flags: noImpl,
+        fd_fdstat_set_rights: noImpl,        
+        fd_filestat_get: noImpl,
+        fd_filestat_set_size: noImpl,
+        fd_filestat_set_times: noImpl,
+        fd_prestat_get: noImpl,
+        fd_prestat_dir_name: noImpl,
+        path_create_directory: noImpl,
+        path_filestat_get: noImpl,
+        path_filestat_set_times: noImpl,
+        path_link: noImpl,
+        path_open: noImpl,
+        path_readlink: noImpl,
+        path_remove_directory: noImpl,
+        path_rename: noImpl,
+        path_symlink: noImpl,
+        path_unlink_file: noImpl,       
+        poll_oneoff: noImpl,
+        proc_exit: () => {},
         random_get: (buf, buf_len) => {
           const dv = new DataView(this.memory.buffer, buf, buf_len);
           for (let i = 0; i < buf_len; i++) {
@@ -6765,10 +6805,11 @@ class WebAssemblyEnvironment extends Environment {
           }
           return 0;
         },
-        proc_exit: () => {},
-        path_open: () => 1,
-        fd_read: () => 1,
-        fd_close: () => 1,
+        sched_yield: noImpl,
+        sock_accept: noImpl,
+        sock_recv: noImpl,
+        sock_send: noImpl,
+        sock_shutdown: noImpl,        
       };  
     }
   }
