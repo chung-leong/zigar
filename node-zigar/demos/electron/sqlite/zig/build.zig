7776e5f2a5dcac39fb2999b3febd409fb2b73780
Chung Leong <chernyshevsky@hotmail.com>
2024-06-10 22:30:15 +0200

    Began work on SQLite example (#386)

---
+++ node-zigar/demos/electron/sqlite/zig/build.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const cfg = @import("./build-cfg.zig");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const lib = b.addSharedLibrary(.{
+        .name = cfg.module_name,
+        .root_source_file = .{ .cwd_relative = cfg.stub_path },
+        .target = target,
+        .optimize = optimize,
+    });
+    const sqlite = b.dependency("sqlite", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const imports = .{
+        .{ .name = "sqlite", .module = sqlite.module("sqlite") },
+    };
+    const mod = b.createModule(.{
+        .root_source_file = .{ .cwd_relative = cfg.module_path },
+        .imports = &imports,
+    });
+    mod.addIncludePath(.{ .cwd_relative = cfg.module_dir });
+    lib.root_module.addImport("module", mod);
+    if (cfg.use_libc) {
+        lib.linkLibC();
+    }
+    const wf = b.addWriteFiles();
+    wf.addCopyFileToSource(lib.getEmittedBin(), cfg.output_path);
+    wf.step.dependOn(&lib.step);
+    b.getInstallStep().dependOn(&wf.step);
+}
