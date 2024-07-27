d3e6af3a6dc15544105fb63fe720df95c67941b4
Chung Leong <chernyshevsky@hotmail.com>
2024-07-27 10:36:09 +0200

    Adjusted snprintf example

--- node-zigar/demos/node/snprintf/src/fprintf.js
+++ node-zigar/demos/node/snprintf/src/fprintf.js
@@ -1,8 +1,10 @@
 require('node-zigar/cjs');
 const { fopen, fclose, fprintf, I8, I16, Usize } = require('../zig/functions.zig');
+const { readFileSync } = require('fs');
 
 const format = 'hello world %hhd %hd %zx\n';
 const args = [ new I8(123), new I16(1234), new Usize(0xFFFF_FFFFn) ];
 const f = fopen('hello.txt', 'w');
 fprintf(f, format, ...args);
 fclose(f);
+console.log(readFileSync('hello.txt', 'utf8'));
