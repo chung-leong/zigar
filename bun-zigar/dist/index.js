e78408659854c59088b7462cfbda1de2e1527526
Chung Leong <chernyshevsky@hotmail.com>
2024-08-08 00:29:01 +0200

    Began work on bun-zigar (#399)

---
+++ bun-zigar/dist/index.js
@@ -0,0 +1,68 @@
+import { plugin } from 'bun';
+import { buildAddon, createEnvironment, getLibraryPath } from 'node-zigar-addon';
+import { dirname, extname, join, parse } from 'path';
+import { pathToFileURL } from 'url';
+import {
+  compile,
+  findConfigFile, findSourceFile, generateCode, getArch, getCachePath,
+  getModuleCachePath, getPlatform, loadConfigFile,
+  optionsForCompile
+} from 'zigar-compiler';
+import { hideStatus, showStatus } from './status.js';
+
+await plugin({
+  name: "ZIG",
+  async setup(build) {
+    build.onLoad({ filter: /\.(zig|zigar)$/ }, async ({ path }) => {
+      const platform = getPlatform();
+      const arch = getArch();
+      const options = {
+        clean: false,
+        optimize: 'Debug',
+        platform,
+        arch,
+      };
+      const configPath = await findConfigFile('bun-zigar.config.json', dirname(path));
+      if (configPath) {
+        // add options from config file
+        Object.assign(options, await loadConfigFile(configPath, optionsForCompile));
+      }
+      const ext = extname(path);
+      const srcPath = (ext === '.zig') ? path : findSourceFile(path, options);
+      const modPath = (ext === '.zig') ? getModuleCachePath(path, options) : path;
+      const addonParentDir = (ext === '.zig') ? getCachePath(options) : dirname(path);
+      const addonDir = join(addonParentDir, 'node-zigar-addon');
+      // build the Node-API addon if necessary
+      const addonOptions = { recompile: true };
+      if (!options.quiet) {
+        const modName = parse(path).name;
+        Object.assign(options, {
+          onStart: () => showStatus(`Building module "${modName}"`),
+          onEnd: () => hideStatus(),
+        });
+        Object.assign(addonOptions, {
+          onStart: () => showStatus(`Building Bun.js addon (${platform}/${arch})`),
+          onEnd: () => hideStatus(),
+        });
+      }
+      const { outputPath: addonPath } = await buildAddon(addonDir, addonOptions);
+      // compile the module if srcPath isn't undefined; if it is, then compile() will simply return
+      // the path to the matching so/dylib/dll file in modPath; basically, when node-zigar.config.json
+      // is absent, compilation does not occur
+      const { outputPath } = await compile(srcPath, modPath, options);
+      const env = createEnvironment({ addonPath });
+      env.loadModule(outputPath);
+      env.acquireStructures(options);
+      const definition = env.exportStructures();
+      // get the absolute path to node-zigar-addon so the transpiled code can find it
+      const runtimeURL = pathToFileURL(getLibraryPath()).href;
+      const binarySource = env.hasMethods() ? JSON.stringify(outputPath) : undefined;
+      const envOptions = { addonPath };
+      const { code } = generateCode(definition, { runtimeURL, binarySource, envOptions });
+      return {
+        contents: code,
+        loader: 'js',
+      };
+    });
+  },
+});
---
+++ bun-zigar/dist/status.js
@@ -0,0 +1,75 @@
+import { execSync } from 'child_process';
+import { write } from 'fs';
+import { platform } from 'os';
+import { isatty } from 'tty';
+
+const statusCharacters = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏';
+let statusClear;
+
+export function showStatus(message) {
+  const fd = 2;
+  const tty = isatty(fd);
+  let currentCP;
+  hideStatus();
+  if (tty) {
+    /* c8 ignore start */
+    if (platform() === 'win32') {
+      try {
+        const m = /\d+/.exec(execSync('chcp').toString().trim());
+        if (m) {
+          currentCP = parseInt(m[0]);
+          if (currentCP !== 65001) {
+            execSync('chcp 65001');
+          }
+        }
+      } catch (err) {
+      }
+    }
+    /* c8 ignore end */
+    let pos = 0;
+    const update = () => {
+      const c = statusCharacters.charAt(pos++);
+      if (pos >= statusCharacters.length) {
+        pos = 0;
+      }
+      write(fd, `\r\x1b[33m${c}\x1b[0m ${message}`, () => {});
+    };
+    const interval = setInterval(update, 150);
+    statusClear = () => {
+      clearInterval(interval);
+      write(fd, '\r\x1b[K', () => {});
+      /* c8 ignore start */
+      if (currentCP && currentCP !== 65001) {
+        try {
+          execSync(`chcp ${currentCP}`);
+        } catch (err){
+        }
+      }
+      /* c8 ignore end */
+    };
+    update();
+    /* c8 ignore next 6 */
+  } else {
+    write(fd, message, () => {});
+    statusClear = () => {
+      write(fd, `\b \b`.repeat(message.length), () => {});
+    };
+  }
+}
+
+export function hideStatus() {
+  statusClear?.();
+  statusClear = null;
+}
+
+export function showResult(message) {
+  const fd = 2;
+  const tty = isatty(fd);
+  const c = '✓';
+  if (tty) {
+    write(fd, `\r\x1b[32m${c}\x1b[0m ${message}\n`, () => {});
+    /* c8 ignore next 3 */
+  } else {
+    write(fd, `${message}\n`, () => {});
+  }
+}
