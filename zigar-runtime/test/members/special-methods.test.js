5801fe857e21f7d286349a0d7e5615ce6b96ca17
Chung Leong <chernyshevsky@hotmail.com>
2024-09-16 17:24:29 +0200

    Improved test coverage (#347)

---
+++ zigar-runtime/test/members/special-methods.test.js
@@ -0,0 +1,178 @@
+import { expect } from 'chai';
+import { MemberType, StructureFlag, StructureType } from '../../src/constants.js';
+import { defineEnvironment } from '../../src/environment.js';
+import '../../src/mixins.js';
+
+const Env = defineEnvironment();
+
+describe('Member: special-methods', function() {
+  describe('defineSpecialMethods', function() {
+    it('should return descriptors for toJSON and valueOf', function() {
+      const env = new Env();
+      const descriptors = env.defineSpecialMethods();
+      expect(descriptors.toJSON.value).to.be.a('function');
+      expect(descriptors.valueOf.value).to.be.a('function');
+    })
+  })
+  describe('defineStructure', function() {
+    it('should attach toJSON and valueOf methods to defined structures', function() {
+      const env = new Env();
+      const structStructure = env.beginStructure({
+        type: StructureType.Struct,
+        flags: StructureFlag.HasObject | StructureFlag.HasSlot,
+        name: 'Hello',
+        byteSize: 4 * 2,
+      });
+      env.attachMember(structStructure, {
+        name: 'dog',
+        type: MemberType.Int,
+        isRequired: true,
+        byteSize: 4,
+        bitOffset: 0,
+        bitSize: 32,
+        structure: {},
+      });
+      env.attachMember(structStructure, {
+        name: 'cat',
+        type: MemberType.Int,
+        isRequired: true,
+        byteSize: 4,
+        bitOffset: 32,
+        bitSize: 32,
+        structure: {},
+      });
+      env.defineStructure(structStructure);
+      env.endStructure(structStructure);
+      const arrayStructure = env.beginStructure({
+        type: StructureType.Array,
+        flags: StructureFlag.HasObject | StructureFlag.HasSlot,
+        name: 'HelloArray',
+        length: 4,
+        byteSize: structStructure.byteSize * 4,
+      });
+      env.attachMember(arrayStructure, {
+        type: MemberType.Object,
+        bitSize: 64,
+        byteSize: 8,
+        structure: structStructure,
+      });
+      env.defineStructure(arrayStructure);
+      env.endStructure(arrayStructure);
+      const structure = env.beginStructure({
+        type: StructureType.Struct,
+        flags: StructureFlag.HasObject | StructureFlag.HasSlot,
+        name: 'Complex',
+        byteSize: arrayStructure.byteSize + 8 * 2,
+      });
+      env.attachMember(structure, {
+        name: 'animals',
+        type: MemberType.Object,
+        bitSize: arrayStructure.byteSize * 8,
+        bitOffset: 0,
+        byteSize: arrayStructure.byteSize,
+        structure: arrayStructure,
+        slot: 0,
+        structure: arrayStructure,
+      });
+      env.attachMember(structure, {
+        name: 'donut',
+        type: MemberType.Float,
+        bitSize: 64,
+        bitOffset: arrayStructure.byteSize * 8,
+        byteSize: 8,
+        structure: {},
+      })
+      env.attachMember(structure, {
+        name: 'turkey',
+        type: MemberType.Float,
+        bitSize: 64,
+        bitOffset: (arrayStructure.byteSize + 8) * 8,
+        byteSize: 8,
+        structure: {},
+      });
+      const Complex = env.defineStructure(structure);
+      env.endStructure(structure);
+      const data = {
+        animals: [
+          { dog: 1, cat: 2 },
+          { dog: 3, cat: 4 },
+          { dog: 5, cat: 6 },
+          { dog: 7, cat: 8 },
+        ],
+        donut: 3.5,
+        turkey: 1e7,
+      };
+      const object = new Complex(data);
+      expect(JSON.stringify(object)).to.equal(JSON.stringify(data));
+      expect(object.valueOf()).to.eql(data);
+    })
+    it('should replace inaccessible pointer with symbol', function() {
+      const env = new Env();
+      const floatStructure = env.beginStructure({
+        type: StructureType.Primitive,
+        name: 'f64',
+        byteSize: 8,
+      });
+      env.attachMember(floatStructure, {
+        type: MemberType.Float,
+        bitSize: 64,
+        bitOffset: 0,
+        byteSize: 8,
+        structure: floatStructure,
+      });
+      env.defineStructure(floatStructure);
+      env.endStructure(floatStructure);
+      const ptrStructure = env.beginStructure({
+        type: StructureType.Pointer,
+        name: '*f64',
+        byteSize: 8,
+      });
+      env.attachMember(ptrStructure, {
+        type: MemberType.Object,
+        bitSize: 64,
+        byteSize: 8,
+        bitOffset: 0,
+        structure: floatStructure,
+        slot: 0,
+      });
+      env.defineStructure(ptrStructure);
+      env.endStructure(ptrStructure);
+      const structure = env.beginStructure({
+        type: StructureType.Union,
+        flags: StructureFlag.HasObject | StructureFlag.HasSlot,
+        name: 'Union',
+        byteSize: 8,
+      });
+      env.attachMember(structure, {
+        name: 'goat',
+        type: MemberType.Object,
+        bitSize: 64,
+        bitOffset: 0,
+        byteSize: 8,
+        structure: ptrStructure,
+        slot: 0,
+      });
+      env.attachMember(structure, {
+        name: 'donut',
+        type: MemberType.Float,
+        bitSize: 64,
+        bitOffset: 0,
+        byteSize: 8,
+        structure: floatStructure,
+      });
+      env.attachMember(structure, {
+        name: 'turkey',
+        type: MemberType.Float,
+        bitSize: 64,
+        bitOffset: 0,
+        byteSize: 8,
+        structure: floatStructure,
+      });
+      const Complex = env.defineStructure(structure);
+      env.endStructure(structure);
+      const object = new Complex({ donut: 3.5 });
+      expect(object.valueOf().goat).to.be.a('symbol');
+      expect(JSON.stringify(object)).to.equal(JSON.stringify({ donut: 3.5, turkey: 3.5 }));
+    })
+  })
+})
--- zigar-runtime/test/members/special-props.test.js
+++ zigar-runtime/test/members/special-props.test.js
@@ -1,19 +1,141 @@
+import { expect } from 'chai';
+import { MemberType, StructureFlag, StructureType } from '../../src/constants.js';
 import { defineEnvironment } from '../../src/environment.js';
 import '../../src/mixins.js';
 
 const Env = defineEnvironment();
 
-describe('isTypedArray', function() {
-  // it('should return true when given the correct TypedArray', function() {
-  //   const ta = new Int32Array(4);
-  //   expect(isTypedArray(ta, Int32Array)).to.be.true;
-  // })
-  // it('should return false when the array type is different', function() {
-  //   const ta = new Int32Array(4);
-  //   expect(isTypedArray(ta, Uint32Array)).to.be.false;
-  // })
-  // it('should return false when given no array type', function() {
-  //   const ta = new Int32Array(4);
-  //   expect(isTypedArray(ta)).to.be.false;
-  // })
+describe('Member: special-props', function() {
+  describe('defineSpecialProperties', function() {
+    it('should return descriptors for special props', function() {
+      const env = new Env();
+      const intStructure = env.beginStructure({
+        type: StructureType.Primitive,
+        name: 'u8',
+        byteSize: 1,
+      });
+      env.attachMember(intStructure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        bitOffset: 0,
+        byteSize: 1,
+        structure: intStructure,
+      });
+      env.defineStructure(intStructure);
+      env.finalizeStructure(intStructure);
+      const structure = env.beginStructure({
+        type: StructureType.Array,
+        flags: StructureFlag.IsString,
+        name: '[11]u8',
+        length: 11,
+        byteSize: 11,
+      });
+      env.attachMember(structure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        byteSize: 1,
+        structure: intStructure
+      });
+      const descriptors = env.defineSpecialProperties(structure);
+      expect(descriptors.dataView.get).to.be.a('function');
+      expect(descriptors.dataView.set).to.be.a('function');
+      expect(descriptors.base64.get).to.be.a('function');
+      expect(descriptors.base64.set).to.be.a('function');
+      expect(descriptors.typedArray.get).to.be.a('function');
+      expect(descriptors.typedArray.set).to.be.a('function');
+      expect(descriptors.string.get).to.be.a('function');
+      expect(descriptors.string.set).to.be.a('function');
+    })
+  })
+  describe('defineStructure', function() {
+    it('should attach special props to structures', function() {
+      const env = new Env();
+      const intStructure = env.beginStructure({
+        type: StructureType.Primitive,
+        name: 'u8',
+        byteSize: 1,
+      });
+      env.attachMember(intStructure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        bitOffset: 0,
+        byteSize: 1,
+        structure: intStructure,
+      });
+      env.defineStructure(intStructure);
+      env.finalizeStructure(intStructure);
+      const structure = env.beginStructure({
+        type: StructureType.Array,
+        flags: StructureFlag.IsString,
+        name: '[11]u8',
+        length: 11,
+        byteSize: 11,
+      });
+      env.attachMember(structure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        byteSize: 1,
+        structure: intStructure
+      });
+      const Array = env.defineStructure(structure);
+      env.finalizeStructure(structure);
+      const array = new Array([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ]);
+      expect(array.dataView.byteLength).to.equal(11);
+      expect(array.typedArray).to.be.a('Uint8Array');
+      expect(array.typedArray[3]).to.equal(4);
+      array.typedArray = new Uint8Array([ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 ]);
+      expect([ ...array ]).to.eql([ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 ]);
+      const { base64 } = array;
+      expect(base64).to.be.a('string');
+      const array2 = new Array({ base64 });
+      expect([ ...array2 ]).to.eql([ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110 ]);
+      for (let i = 0, c = 'A'.charCodeAt(0); i < 11; i++, c++) {
+        array[i] = c;
+      }
+      expect(array.string).to.equal('ABCDEFGHIJK');
+      array.string = 'Hello world';
+      expect(array.string).to.equal('Hello world');
+    })
+    it('should throw when special props are given incorrect data', function() {
+      const env = new Env();
+      const intStructure = env.beginStructure({
+        type: StructureType.Primitive,
+        name: 'u8',
+        byteSize: 1,
+      });
+      env.attachMember(intStructure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        bitOffset: 0,
+        byteSize: 1,
+        structure: intStructure,
+      });
+      env.defineStructure(intStructure);
+      env.finalizeStructure(intStructure);
+      const structure = env.beginStructure({
+        type: StructureType.Array,
+        flags: StructureFlag.IsString,
+        name: '[11]u8',
+        length: 11,
+        byteSize: 11,
+      });
+      env.attachMember(structure, {
+        type: MemberType.Uint,
+        bitSize: 8,
+        byteSize: 1,
+        structure: intStructure
+      });
+      const Array = env.defineStructure(structure);
+      env.finalizeStructure(structure);
+      const array = new Array([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ]);
+      expect(() => array.typedArray = new Uint8Array(12)).to.throw(TypeError);
+      expect(() => array.dataView = new DataView(new ArrayBuffer(0))).to.throw(TypeError);
+      expect(() => array.dataView = new ArrayBuffer(0)).to.throw(TypeError);
+      expect(() => array.base64 = '').to.throw(TypeError);
+      expect(() => array.base64 = 5).to.throw(TypeError);
+      expect(() => array.string = '').to.throw(TypeError);
+      expect(() => array.string = 123).to.throw(TypeError);
+      expect(() => array.string = 'Hello world!!!').to.throw(TypeError);
+    })
+  })
 })
