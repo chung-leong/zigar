9c24ba17d1223cf69a8063c71f96937a7d4a45b9
Chung Leong <chernyshevsky@hotmail.com>
2024-11-05 23:24:23 +0100

    Fixed test case

--- zigar-compiler/test/integration/function-calling/accept-u8.zig
+++ zigar-compiler/test/integration/function-calling/accept-u8.zig
@@ -1,7 +1,7 @@
 pub fn accept1(_: u8) void {}
-pub fn accept2(_: u8) void {}
-pub fn accept3(_: u8, _: u8) void {}
-pub fn accept4(_: u8, _: u8) void {}
+pub fn accept2(_: u8, _: u8) void {}
+pub fn accept3(_: u8, _: u8, _: u8) void {}
+pub fn accept4(_: u8, _: u8, _: u8, _: u8) void {}
 
 pub const Struct = struct {
     pub fn accept(_: @This(), _: u8, _: u8) void {}
