70b9a15de18cf7695154e59f4e0a461cba8f5436
Chung Leong <chernyshevsky@hotmail.com>
2024-11-27 18:28:43 +0100

    Changed test sample for coverage purpose

--- zigar-compiler/test/zig-samples/basic/fn-pointer.zig
+++ zigar-compiler/test/zig-samples/basic/fn-pointer.zig
@@ -5,3 +5,8 @@ pub const Fn = fn () void;
 pub fn call(ptr: *const Fn) void {
     ptr();
 }
+
+// to ensure that wasi_snapshot_preview1 exists as an import for test coverage purpose
+pub fn hello() void {
+    std.debug.print("Hello world\n", .{});
+}
