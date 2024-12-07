30299335ab40697afeead87bff7e709f6fde4047
Chung Leong <chernyshevsky@hotmail.com>
2024-12-07 21:59:28 +0100

    Updated ziglua test case

--- zigar-compiler/test/integration/package-manager/use-ziglua/build.zig
+++ zigar-compiler/test/integration/package-manager/use-ziglua/build.zig
@@ -1,20 +1,34 @@
 const std = @import("std");
-const cfg = @import("./build-cfg.zig");
+const builtin = @import("builtin");
+const cfg = @import("build-cfg.zig");
+
+const host_type = if (cfg.is_wasm) "wasm" else "napi";
+const zig_path = std.fmt.comptimePrint("{s}{d}.{d}{c}", .{
+    cfg.zigar_src_path,
+    builtin.zig_version.major,
+    builtin.zig_version.minor,
+    std.fs.path.sep,
+});
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const lib = b.addSharedLibrary(.{
         .name = cfg.module_name,
-        .root_source_file = .{ .cwd_relative = cfg.stub_path },
+        .root_source_file = .{ .cwd_relative = zig_path ++ "stub-" ++ host_type ++ ".zig" },
         .target = target,
         .optimize = optimize,
+        .single_threaded = !cfg.multithreaded,
+    });
+    const zigar = b.createModule(.{
+        .root_source_file = .{ .cwd_relative = zig_path ++ "zigar.zig" },
     });
     const ziglua = b.dependency("ziglua", .{
         .target = target,
         .optimize = optimize,
     });
-    const imports = .{
+    const imports = [_]std.Build.Module.Import{
+        .{ .name = "zigar", .module = zigar },
         .{ .name = "ziglua", .module = ziglua.module("ziglua") },
     };
     const mod = b.createModule(.{
@@ -23,6 +37,9 @@ pub fn build(b: *std.Build) void {
     });
     mod.addIncludePath(.{ .cwd_relative = cfg.module_dir });
     lib.root_module.addImport("module", mod);
+    if (cfg.use_libc) {
+        lib.linkLibC();
+    }
     if (cfg.is_wasm) {
         // WASM needs to be compiled as exe
         lib.kind = .exe;
@@ -30,9 +47,9 @@ pub fn build(b: *std.Build) void {
         lib.entry = .disabled;
         lib.rdynamic = true;
         lib.wasi_exec_model = .reactor;
-    }
-    if (cfg.use_libc) {
-        lib.linkLibC();
+        lib.import_memory = true;
+        lib.import_table = true;
+        lib.max_memory = cfg.max_memory;
     }
     const wf = switch (@hasDecl(std.Build, "addUpdateSourceFiles")) {
         true => b.addUpdateSourceFiles(),
