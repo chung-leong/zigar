aedec9031b383d8bdf2b6cd5a7c879d7b418f6fa
Chung Leong <chernyshevsky@hotmail.com>
2024-12-13 17:17:25 +0100

    Fixed text decoding (#523)

--- zigar-runtime/src/utils.js
+++ zigar-runtime/src/utils.js
@@ -68,6 +68,9 @@ export function decodeText(arrays, encoding = 'utf-8') {
   } else {
     array = arrays;
   }
+  if (array.buffer[Symbol.toStringTag] === 'SharedArrayBuffer') {
+    array = new array.constructor(array);
+  }
   return decoder.decode(array);
 }
 
