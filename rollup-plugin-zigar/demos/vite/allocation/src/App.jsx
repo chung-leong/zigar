1de2a7fc658b4837f79bf517be8c632c7f6d1907
Chung Leong <chernyshevsky@hotmail.com>
2025-02-03 23:14:14 +0100

    Added WASM allocation demo

---
+++ rollup-plugin-zigar/demos/vite/allocation/src/App.jsx
@@ -0,0 +1,66 @@
+import { useCallback, useEffect, useState } from 'react'
+import { runTest, startThreads } from '../zig/allocation.zig'
+
+const threadCount = 16;
+
+function App() {
+  const [ counts, setCounts ] = useState(() => {
+    const array = [ 0n ];
+    for (let i = 0n; i < threadCount; i++) array.push(0n);
+    return array;
+  });
+  const [ controller ] = useState(new AbortController);
+  const [ terminated, setTerminated ] = useState(false);
+  const [ text, setText ] = useState('');
+  const [ pos, setPos ] = useState(0);
+  const [ duration, setDuration ] = useState(0);
+  const onStop = useCallback(() => controller.abort(), [ controller ]);
+  const onPosChange = useCallback(evt => setPos(evt.target.value), []);
+  const onTextChange = useCallback(evt => setText(evt.target.value), []);
+  useEffect(() => {
+    const { signal } = controller;
+    const report = (id, count) => {
+      setCounts((prevCounts) => {
+        const newCounts = prevCounts.slice();
+        newCounts[id] = count;
+        return newCounts;
+      });
+    };
+    startThreads(threadCount, 5000, report, { signal }).then(() => setTerminated(true));
+    var mainCount = 0n;
+    const interval = setInterval(() => {
+      const start = performance.now();
+      mainCount = runTest(mainCount, 100);
+      const end = performance.now();
+      setDuration(Math.ceil(end - start));
+      report(0n, mainCount);
+    }, 250);
+    signal.addEventListener('abort', () => clearInterval(interval));
+  }, []);
+  return (
+    <>
+      <ul>
+        {
+          counts.map((count, id) => {
+            const name = (id === 0) ? `Main thread` : `Thread #${id}`;
+            const text = (id === 0) ? `${count} (${duration} ms)` : `${count}`;
+            return (
+              <li key={id}>{name}: {text}</li>
+            )
+          })
+        }
+      </ul>
+      <div>
+        <button onClick={onStop} disabled={terminated}>Stop</button>
+      </div>
+      <div>
+        <input onChange={onPosChange} type="range" min="0" max="5000" value={pos} />
+      </div>
+      <div>
+        <textarea onChange={onTextChange} value={text} width="80" height="40" />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ rollup-plugin-zigar/demos/vite/allocation/zig/allocation.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+const zigar = @import("zigar");
+
+const Reporter = fn (usize, usize) void;
+const allocator = zigar.mem.getDefaultAllocator();
+
+pub fn runTest(count: usize, iterations: usize) !usize {
+    var prng = std.Random.Xoshiro256.init(count);
+    var random = std.Random.init(&prng, std.Random.Xoshiro256.fill);
+    var i: usize = 0;
+    while (i < iterations) : (i += 1) {
+        const random_amount = random.uintAtMost(usize, 8 * 1024);
+        const slice = try allocator.alloc(u8, random_amount);
+        allocator.free(slice);
+    }
+    return count + i;
+}
+
+pub fn startThreads(
+    count: usize,
+    iterations: usize,
+    reporter: *const Reporter,
+    signal: zigar.function.AbortSignal,
+    promise: zigar.function.Promise(void),
+) !void {
+    const multipart_promise = try promise.partition(allocator, count);
+    for (0..count) |i| {
+        const thread = try std.Thread.spawn(.{
+            .allocator = allocator,
+            .stack_size = 64 * 1024,
+        }, threadFn, .{
+            i + 1,
+            iterations,
+            reporter,
+            signal,
+            multipart_promise,
+        });
+        thread.detach();
+    }
+}
+
+fn threadFn(
+    id: usize,
+    iterations: usize,
+    reporter: *const Reporter,
+    signal: zigar.function.AbortSignal,
+    promise: zigar.function.Promise(void),
+) void {
+    var count: usize = 0;
+    while (signal.off()) {
+        if (runTest(count, iterations)) |new_count| {
+            count = new_count;
+            reporter(id, count);
+        } else |_| {
+            break;
+        }
+    }
+    promise.resolve({});
+}
