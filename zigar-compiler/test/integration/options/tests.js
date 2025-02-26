4318a815a32c4d2eb41b84be049e99cba020be37
Chung Leong <chernyshevsky@hotmail.com>
2025-02-26 12:04:17 +0100

    Fixed options test (#587)

--- zigar-compiler/test/integration/options/tests.js
+++ zigar-compiler/test/integration/options/tests.js
@@ -7,29 +7,19 @@ export function addTests(importModule, options) {
       return importModule(url, options);
   };
   describe('Options', function() {
-    skip.
     it('should omit functions when option is set', async function() {
       this.timeout(0);
       const { default: moduleWO } = await importTest('omit-functions', { omitFunctions: true });
       expect(moduleWO.a).to.be.undefined;
       expect(moduleWO.b).to.be.undefined;
       expect(moduleWO.c).to.be.undefined;
-      const { default: moduleW } = await importTest('omit-functions', { omitFunctions: false });
-      expect(moduleW.a).to.be.a('function');
-      expect(moduleW.b).to.be.a('function');
-      expect(moduleW.c).to.be.a('function');
     })
-    skip.
     it('should omit variables when option is set', async function() {
       this.timeout(0);
       const { default: moduleWO } = await importTest('omit-variables', { omitVariables: true });
       expect(moduleWO.a).to.be.a('number');
       expect(moduleWO.b).to.be.undefined;
       expect(moduleWO.c).to.be.undefined;
-      const { default: moduleW } = await importTest('omit-variables', { omitVariables: false });
-      expect(moduleW.a).to.be.a('number');
-      expect(moduleW.b).to.be.a('number');
-      expect(moduleW.c).to.be.a('number');
     })
   })
 }
