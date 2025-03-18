d352180e0c6954c8db9b0571f466793d6096148b
Chung Leong <chernyshevsky@hotmail.com>
2025-03-18 21:53:10 +0100

    Updated zzz example to the latest version (#549)

--- node-zigar/demos/node/zzz/zig/server.zig
+++ node-zigar/demos/node/zzz/zig/server.zig
@@ -1,91 +1,119 @@
 const std = @import("std");
-const builtin = @import("builtin");
-const zigar = @import("zigar");
+const log = std.log.scoped(.@"examples/basic");
 
 const zzz = @import("zzz");
 const http = zzz.HTTP;
 
 const tardy = zzz.tardy;
-const Tardy = tardy.Tardy(switch (builtin.target.os.tag) {
-    .linux => .io_uring,
-    .windows => .busy_loop,
-    else => .kqueue,
-});
+const Tardy = tardy.Tardy(.auto);
 const Runtime = tardy.Runtime;
+const Socket = tardy.Socket;
 
 const Server = http.Server;
 const Router = http.Router;
 const Context = http.Context;
 const Route = http.Route;
+const Respond = http.Respond;
 
-const Responder = *const fn (std.mem.Allocator, []const u8) error{Unexpected}![]u8;
+const zigar = @import("zigar");
 
-var responders: [4]?Responder = .{ null, null, null, null };
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const allocator = gpa.allocator();
+pub fn startServer(host: []const u8, port: u16, promise: zigar.function.PromiseOf(main)) !void {
+    try zigar.thread.use();
+    const thread = try std.Thread.spawn(.{}, main, .{ host, port, promise.any() });
+    thread.detach();
+}
 
-fn page_handler(ctx: *Context, id: usize) !void {
-    if (responders[id]) |f| {
-        if (f(ctx.allocator, ctx.request.uri orelse "")) |data| {
-            return try ctx.respond(.{
+const ContentFn = fn (std.mem.Allocator, []const u8) error{Unexpected}![]u8;
+var base_content_fn: ?*const ContentFn = null;
+
+fn base_handler(ctx: *const Context, _: void) !Respond {
+    if (base_content_fn) |f| {
+        if (f(ctx.allocator, ctx.request.uri orelse "")) |body| {
+            return ctx.response.apply(.{
                 .status = .OK,
                 .mime = http.Mime.HTML,
-                .body = data,
+                .body = body,
             });
-        } else |_| {
-            std.debug.print("Responder failed\n", .{});
-        }
+        } else |_| {}
     }
-    try ctx.respond(.{ .status = .@"Service Unavailable" });
+    return ctx.response.apply(.{
+        .status = .@"Service Unavailable",
+        .mime = http.Mime.TEXT,
+        .body = "Service Unavailable",
+    });
 }
 
-fn runServer(host: []const u8, port: u16) !void {
-    defer allocator.free(host);
-    var t = try Tardy.init(.{
-        .allocator = allocator,
-        .threading = .auto,
+pub fn setBaseHandler(f: ?*const ContentFn) void {
+    if (base_content_fn) |ex_f| zigar.function.release(ex_f);
+    base_content_fn = f;
+}
+
+var cat_content_fn: ?*const ContentFn = null;
+
+fn cat_handler(ctx: *const Context, _: void) !Respond {
+    if (cat_content_fn) |f| {
+        if (f(ctx.allocator, ctx.request.uri orelse "")) |body| {
+            return ctx.response.apply(.{
+                .status = .OK,
+                .mime = http.Mime.HTML,
+                .body = body,
+            });
+        } else |_| {}
+    }
+    return ctx.response.apply(.{
+        .status = .@"Service Unavailable",
+        .mime = http.Mime.TEXT,
+        .body = "Service Unavailable",
     });
+}
+
+pub fn setCatHandler(f: ?*const ContentFn) void {
+    if (cat_content_fn) |ex_f| zigar.function.release(ex_f);
+    cat_content_fn = f;
+}
+
+fn main(host: []const u8, port: u16, promise: zigar.function.Promise(anyerror!void)) !void {
+    errdefer |err| promise.resolve(err);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var t = try Tardy.init(allocator, .{ .threading = .auto });
     defer t.deinit();
+
     var router = try Router.init(allocator, &.{
-        Route.init("/").get(@as(usize, 0), page_handler).layer(),
-        Route.init("/cat").get(@as(usize, 1), page_handler).layer(),
+        Route.init("/").get({}, base_handler).layer(),
+        Route.init("/cat").get({}, cat_handler).layer(),
     }, .{});
     defer router.deinit(allocator);
-    const ServerConfig = struct {
-        host: []const u8,
-        port: u16,
-        router: *Router,
-    };
-    const config: ServerConfig = .{
-        .host = host,
-        .port = port,
-        .router = &router,
-    };
-    const task = struct {
-        fn entry(rt: *Runtime, cfg: *const ServerConfig) !void {
-            var server = Server.init(rt.allocator, .{});
-            try server.bind(.{ .ip = .{ .host = cfg.host, .port = cfg.port } });
-            try server.serve(cfg.router, rt);
-        }
 
-        fn exit(rt: *Runtime, _: void) !void {
-            try Server.clean(rt);
-        }
+    // create socket for tardy
+    var socket = try Socket.init(.{ .tcp = .{ .host = host, .port = port } });
+    defer socket.close_blocking();
+    try socket.bind();
+    try socket.listen(4096);
+
+    const EntryParams = struct {
+        router: *const Router,
+        socket: Socket,
+        promise: zigar.function.Promise(anyerror!void),
     };
-    try t.entry(&config, task.entry, void{}, task.exit);
-}
 
-pub fn startServer(host: []const u8, port: u16) !void {
-    const host_copy = try allocator.dupe(u8, host);
-    try zigar.thread.use();
-    _ = try std.Thread.spawn(.{ .allocator = allocator }, runServer, .{ host_copy, port });
-}
-
-pub fn setResponder(id: usize, f: ?Responder) void {
-    if (id < responders.len) {
-        if (responders[id]) |previous| {
-            zigar.function.release(previous);
-        }
-        responders[id] = f;
-    }
+    t.entry(
+        EntryParams{ .router = &router, .socket = socket, .promise = promise },
+        struct {
+            fn entry(rt: *Runtime, p: EntryParams) !void {
+                var server = Server.init(.{
+                    .stack_size = 1024 * 1024 * 4,
+                    .socket_buffer_bytes = 1024 * 2,
+                    .keepalive_count_max = null,
+                    .connection_count_max = 1024,
+                });
+                p.promise.resolve(
+                    server.serve(rt, p.router, .{ .normal = p.socket }),
+                );
+            }
+        }.entry,
+    ) catch {};
 }
