a8dfb0820e824367fe768c3fa4c82ba94c8015a2
Chung Leong <chernyshevsky@hotmail.com>
2025-03-20 01:46:49 +0100

    Moved Raylib code into thread (#577)

--- node-zigar/demos/node/raylib/zig/raylib.zig
+++ node-zigar/demos/node/raylib/zig/raylib.zig
@@ -2,8 +2,17 @@ const std = @import("std");
 const zigar = @import("zigar");
 const r = @cImport(@cInclude("raylib.h"));
 
-pub fn run(promise: zigar.function.Promise(void)) void {
+pub fn run(promise: zigar.function.Promise(void)) !void {
+    try zigar.thread.use();
+    errdefer zigar.thread.end();
+    const thread = try std.Thread.spawn(.{}, main, .{promise});
+    thread.detach();
+}
+
+fn main(promise: zigar.function.Promise(void)) void {
+    defer zigar.thread.end();
     defer promise.resolve({});
+
     r.InitWindow(800, 450, "raylib [core] example - basic window");
     defer r.CloseWindow();
 
