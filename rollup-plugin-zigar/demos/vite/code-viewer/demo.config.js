2f6f334c73c07471b08cd5bc114d9ac8940acb03
Chung Leong <chernyshevsky@hotmail.com>
2025-05-25 15:59:33 +0200

    Added config file for building demo (#632)

---
+++ rollup-plugin-zigar/demos/vite/code-viewer/demo.config.js
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react-swc';
+import zigar from 'rollup-plugin-zigar';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    zigar({ topLevelAwait: false, useLibc: true, multithreaded: true }),
+  ],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    }
+  },
+  build: {
+    outDir: '../../../../../zigar.website/demos/rollup-plugin-zigar/code-viewer',
+    emptyOutDir: true,
+  },
+})
