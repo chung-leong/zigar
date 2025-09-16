40208b80c1152f13b1f92792561e6cb52d87f90c
Chung Leong <chernyshevsky@hotmail.com>
2025-09-16 02:08:00 +0200

    Added command for applying patch (#717)

--- rollup-plugin-zigar/bin/cli.js
+++ rollup-plugin-zigar/bin/cli.js
@@ -1,15 +1,50 @@
 #!/usr/bin/env node
 
+import { execSync } from 'child_process';
 import { copyFile } from 'fs/promises';
 import { createRequire } from 'module';
 import { join, resolve } from 'path';
 
 async function copyBuildFile() {
-  const require = createRequire(import.meta.url);
-  const modPath = require.resolve('zigar-compiler');
-  const srcPath = resolve(modPath, '../../zig/build.zig');
+  const modPath = findZigarCompiler();
+  const srcPath = join(modPath, 'zig/build.zig');
   const dstPath = join(process.cwd(), 'build.zig');
   await copyFile(srcPath, dstPath);
+}
+
+async function patchStandardLibrary() {
+  const modPath = findZigarCompiler();
+  const patchPath = join(modPath, 'wasi-thread.patch');
+  const zigPath = findZigCompiler();
+  const stdPath = join(zigPath, 'lib/std');  
+  const dryRunCmd = `patch -d "${stdPath}" -t -p0 -N --dry-run < "${patchPath}"`;
+  try {
+    const dryRunResult = execSync(dryRunCmd).toString();
+    const patchCmd = `patch -d "${stdPath}" -t -p0 < "${patchPath}"`;
+    const result = execSync(patchCmd).toString();
+    console.log(result);
+  } catch (err) {
+    const dryRunError = err.stdout.toString();
+    throw new Error(`Operation failed:\n\n${dryRunError}`);
+  }
+}
+
+function findZigarCompiler() {
+  const require = createRequire(import.meta.url);
+  const jsPath = require.resolve('zigar-compiler');
+  return resolve(jsPath, '../..');
+}
+
+function findZigCompiler() {
+  let binPath;
+  if (process.platform === 'win32') {
+    const result = execSync(`where zig`).toString();
+    binPath = result;
+  } else {
+    const result = execSync(`whereis zig`).toString();
+    binPath = result.replace(/^\w+:\s*/, '');
+  }
+  return resolve(binPath, '..');
 }
 
 function printHelp() {
@@ -19,6 +54,7 @@ function printHelp() {
     'Commands:',
     '',
     '  custom        Create a copy of Zigar\'s build.zig in the current folder',
+    '  patch         Patch Zig\'s standard library to enable thread support',
     '  help          Show this message',
     '',
   ];
@@ -32,6 +68,9 @@ try {
   switch (cmd) {
     case 'custom':
       await copyBuildFile();
+      break;
+    case 'patch':
+      await patchStandardLibrary();
       break;
     case 'help':
     case undefined:
