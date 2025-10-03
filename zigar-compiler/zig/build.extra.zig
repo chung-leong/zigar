8f0217cfdce7fffa53928db17b4bd035e468b4f3
Chung Leong <chernyshevsky@hotmail.com>
2025-10-03 17:08:51 +0200

    Added field descriptions to build.extra.zig

--- zigar-compiler/zig/build.extra.zig
+++ zigar-compiler/zig/build.extra.zig
@@ -3,11 +3,22 @@ const std = @import("std");
 pub fn getImports(b: *std.Build, args: anytype) []const std.Build.Module.Import {
     _ = b;
     _ = args;
+    // args contains the following:
+    //
+    //     library: *std.Build.Step.Compile,
+    //     target: std.Build.ResolvedTarget,
+    //     optimize: std.builtin.OptimizeMode,
     return &.{};
 }
 
 pub fn getCSourceFiles(b: *std.Build, args: anytype) []const []const u8 {
     _ = b;
     _ = args;
+    // args contains the following:
+    //
+    //     library: *std.Build.Step.Compile,
+    //     module: *std.Build.Module,
+    //     target: std.Build.ResolvedTarget,
+    //     optimize: std.builtin.OptimizeMode,
     return &.{};
 }
