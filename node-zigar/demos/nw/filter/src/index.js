298afbaa0c9e3a797a283d4392858d1ee04cc352
Chung Leong <chernyshevsky@hotmail.com>
2025-10-30 17:23:17 +0100

    Made all demos use RC 0

--- node-zigar/demos/nw/filter/src/index.js
+++ node-zigar/demos/nw/filter/src/index.js
@@ -3,15 +3,13 @@ const { resolve } = require('path');
 const { pathToFileURL } = require('url');
 const { availableParallelism } = require('os');
 require('node-zigar/cjs');
-const { createOutputAsync, startThreadPool, __zigar } = require('./lib/sepia.zigar');
+const { createOutputAsync, startThreadPool } = require('./lib/sepia.zigar');
 
 const isMac = process.platform === 'darwin'
 
 startThreadPool(availableParallelism());
 
 nw.Window.open('./src/index.html', { width: 800, height: 600, x: 10, y: 10 }, (browser) => {
-  __zigar.connect(browser.window.console);
-
   // handle menu click
   const onOpenClick = () => {
     const { window: { document } } = browser;
