17b4295757b8dbbfae92b68342bb5db9841e8869
Chung Leong <chernyshevsky@hotmail.com>
2025-11-02 18:48:06 +0100

    Appied temporary fix for Safari issue to SQLite demo (#785)

--- rollup-plugin-zigar/demos/vite/sqlite/src/main.jsx
+++ rollup-plugin-zigar/demos/vite/sqlite/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+
+if (typeof(ReadableStreamBYOBReader) !== 'object') {
+  globalThis.ReadableStreamBYOBReader = class {}
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
