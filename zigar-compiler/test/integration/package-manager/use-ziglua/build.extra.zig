756ac492d4c959a6b7986619af0a79785cb68b40
Chung Leong <chernyshevsky@hotmail.com>
2025-11-11 15:01:43 +0100

    Upgraded ziglua test (#799)

--- zigar-compiler/test/integration/package-manager/use-ziglua/build.extra.zig
+++ zigar-compiler/test/integration/package-manager/use-ziglua/build.extra.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 
 pub fn getImports(b: *std.Build, args: anytype) []const std.Build.Module.Import {
-    const lua_wrapper = b.dependency("lua_wrapper", .{
+    const zlua = b.dependency("zlua", .{
         .target = args.target,
         .optimize = args.optimize,
-    }).module("lua_wrapper");
+    }).module("zlua");
     return &.{
-        .{ .name = "lua_wrapper", .module = lua_wrapper },
+        .{ .name = "zlua", .module = zlua },
     };
 }
