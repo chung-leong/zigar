432f95c42dd56dfc9995777b827b72a6415883cb
Chung Leong <chernyshevsky@hotmail.com>
2025-11-23 19:38:10 +0100

    Completely reworked linked-list again (#802)

--- zigar-compiler/zig/type/linked-list.zig
+++ zigar-compiler/zig/type/linked-list.zig
@@ -2,183 +2,196 @@ const std = @import("std");
 const expect = std.testing.expect;
 const expectEqual = std.testing.expectEqual;
 
+fn MarkedPointer(comptime T: type) type {
+    return struct {
+        ptr_state: std.atomic.Value(PtrState) = .init(.{}),
+
+        const State = enum(u2) {
+            previous_in_use,
+            previous_free,
+            previous_allocated,
+        };
+        const state_mask: usize = std.math.maxInt(u2);
+        const PtrState = packed struct(usize) {
+            value: usize = 0,
+
+            pub inline fn init(p: *T, s: State) @This() {
+                return .{ .value = @intFromPtr(p) | @intFromEnum(s) };
+            }
+
+            pub inline fn ptr(self: @This()) *T {
+                return @ptrFromInt(self.value & ~state_mask);
+            }
+
+            pub inline fn state(self: @This()) State {
+                return @enumFromInt(self.value & state_mask);
+            }
+
+            pub inline fn change(self: @This(), s: State) @This() {
+                return .{ .value = (self.value & ~state_mask) | @intFromEnum(s) };
+            }
+
+            pub inline fn isNull(self: @This()) bool {
+                return self.value & ~state_mask == 0;
+            }
+        };
+
+        pub fn load(self: *@This()) PtrState {
+            return self.ptr_state.load(.unordered);
+        }
+
+        pub fn store(self: *@This(), ps: PtrState, comptime order: std.builtin.AtomicOrder) void {
+            self.ptr_state.store(ps, order);
+        }
+
+        pub fn exchange(self: *@This(), expected: PtrState, new: PtrState, comptime order: std.builtin.AtomicOrder) bool {
+            return self.ptr_state.cmpxchgWeak(expected, new, order, .monotonic) == null;
+        }
+    };
+}
+
 pub fn LinkedList(comptime T: type) type {
     return struct {
         const Node = struct {
-            next: std.atomic.Value(*@This()),
-            ref_count: std.atomic.Value(usize),
+            next: MarkedPointer(Node) = .{},
             payload: T,
         };
-        const tail: *Node = @ptrFromInt(std.mem.alignBackward(usize, std.math.maxInt(usize), @alignOf(Node)));
 
-        head: std.atomic.Value(*Node) = .init(tail),
+        head: MarkedPointer(Node) = .{},
         allocator: std.mem.Allocator,
 
         pub fn init(allocator: std.mem.Allocator) @This() {
             return .{ .allocator = allocator };
         }
 
-        pub fn push(self: *@This(), value: T) !*T {
-            const new_node = try self.alloc();
-            new_node.payload = value;
-            new_node.next = .init(tail);
-            // use .release to ensure the payload shows up fully in other CPU cores
-            new_node.ref_count.store(1, .release);
-            self.insert(new_node);
-            return &new_node.payload;
-        }
-
-        fn alloc(self: *@This()) !*Node {
-            var current = self.head.load(.unordered);
-            var current_ptr = &self.head;
-            var prev_deleted = false;
-            while (current != tail) {
-                const next_m = current.next.load(.unordered);
-                const next = getUnmarkedReference(next_m);
-                if (isMarkedReference(next_m)) {
-                    // the next pointer is marked, meaning this node is off the list; since its payload
-                    // might still be in use, we need to make use sure ref_count is zero
-                    if (current.ref_count.cmpxchgWeak(0, 1, .monotonic, .monotonic) == null) {
-                        // now we detach the node from the list altogether by change the previous node's
-                        // next pointer to point to this node's next node
-                        var current_m = current;
-                        var new_next_m = next;
-                        if (prev_deleted) {
-                            // the previous node isn't on the list (but wasn't picked due to ref count);
-                            // mark both pointers
-                            current_m = getMarkedReference(current_m);
-                            new_next_m = getMarkedReference(new_next_m);
+        pub fn push(self: *@This(), payload: T) !void {
+            while (true) {
+                var candidate_node: ?*Node = null;
+                var tail_ptr = &self.head;
+                var current = self.head.load();
+                while (!current.isNull()) {
+                    const next = current.ptr().next.load();
+                    if (next.state() == .previous_free) {
+                        // this node is free
+                        if (candidate_node == null) {
+                            candidate_node = current.ptr();
                         }
-                        current_ptr.*.store(current_m, .unordered);
-                        if (current_ptr.*.cmpxchgWeak(current_m, new_next_m, .monotonic, .monotonic) == null) {
-                            return current;
+                    } else {
+                        // since the new node need to be behind what's on the list already,
+                        // we can't use an unused node that got picked earlier
+                        candidate_node = null;
+                        if (next.isNull()) {
+                            tail_ptr = &current.ptr().next;
+                        }
+                    }
+                    current = next;
+                }
+                if (candidate_node) |node| {
+                    // set the next pointer's state to .previous_allocated first, then copy the payload
+                    const next = node.next.load();
+                    if (node.next.exchange(next, next.change(.previous_allocated), .monotonic)) {
+                        node.payload = payload;
+                        // mark the node as in-play
+                        node.next.store(next.change(.previous_in_use), .release);
+                        return;
+                    } else {
+                        // try again
+                    }
+                } else {
+                    // create a new node
+                    const new_node = try self.allocator.create(Node);
+                    new_node.* = .{ .payload = payload };
+                    while (true) {
+                        const next = tail_ptr.load();
+                        if (next.isNull()) {
+                            if (tail_ptr.exchange(next, .init(new_node, next.state()), .release)) {
+                                return;
+                            } else {
+                                // try again
+                            }
                         } else {
-                            // couldn't make the update (probably because the previous node was removed);
-                            // set the ref count back to zero
-                            current.ref_count.store(0, .unordered);
+                            // another thread has pushed another node just before we did
+                            // try attaching our new node to that one instead
+                            tail_ptr = &next.ptr().next;
                         }
                     }
                 }
-                current_ptr = &current.next;
-                current = next;
-                prev_deleted = isMarkedReference(next_m);
             }
-            // otherwise allocate memory for a new node
-            const new_node = try self.allocator.create(Node);
-            return new_node;
         }
 
-        fn insert(self: *@This(), node: *Node) void {
+        pub fn shift(self: *@This()) ?T {
             while (true) {
-                const head = self.head.load(.unordered);
-                if (head == tail) {
-                    if (self.head.cmpxchgWeak(tail, node, .monotonic, .monotonic) == null) {
-                        break;
+                var current = self.head.load();
+                while (!current.isNull()) {
+                    const next = current.ptr().next.load();
+                    if (next.state() == .previous_in_use) {
+                        // this node is in use; we make a copy of its payload first
+                        const payload = current.ptr().payload;
+                        // then we try to it off the list by changing its next pointer's state
+                        if (current.ptr().next.exchange(next, next.change(.previous_free), .monotonic)) {
+                            return payload;
+                        } else {
+                            // start over, since the exchange failure could be spurious
+                            break;
+                        }
                     }
-                } else {
-                    while (true) {
-                        var current = head;
-                        while (true) {
-                            const next_m = current.next.load(.unordered);
-                            const next = getUnmarkedReference(next_m);
-                            if (next == tail) {
-                                // this node is the last node
-                                var new_next_m = node;
-                                if (isMarkedReference(next_m)) {
-                                    // the node is off the list--its new next pointer also needs to be marked
-                                    new_next_m = getMarkedReference(node);
-                                }
-                                if (current.next.cmpxchgWeak(next_m, new_next_m, .monotonic, .monotonic) == null) {
-                                    return;
+                    current = next;
+                }
+                break;
+            }
+            return null;
+        }
+
+        pub fn find(self: *@This(), comptime match: anytype, arg: anytype) ?T {
+            var current = self.head.load();
+            while (!current.isNull()) {
+                const next = current.ptr().next.load();
+                if (next.state() == .previous_in_use) {
+                    const payload = current.ptr().payload;
+                    // check state again if copying cannot be done atomically
+                    if (@sizeOf(T) <= @sizeOf(usize) or current.ptr().next.load().state() == .previous_in_use) {
+                        if (match(payload, arg)) {
+                            return payload;
+                        }
+                    }
+                }
+                current = next;
+            }
+            return null;
+        }
+
+        pub fn remove(self: *@This(), comptime match: anytype, arg: anytype) ?T {
+            while (true) {
+                var current = self.head.load();
+                while (!current.isNull()) {
+                    const next = current.ptr().next.load();
+                    if (next.state() == .previous_in_use) {
+                        const payload = current.ptr().payload;
+                        if (@sizeOf(T) <= @sizeOf(usize) or current.ptr().next.load().state() == .previous_in_use) {
+                            if (match(payload, arg)) {
+                                if (current.ptr().next.exchange(next, next.change(.previous_free), .monotonic)) {
+                                    return payload;
                                 } else {
                                     // start from beginning again
                                     break;
                                 }
                             }
-                            current = next;
                         }
                     }
+                    current = next;
                 }
-            }
-        }
-
-        pub fn find(self: *@This(), match: anytype, args: anytype) ?*T {
-            var current = self.head.load(.unordered);
-            while (current != tail) {
-                const next_m = current.next.load(.unordered);
-                const next = getUnmarkedReference(next_m);
-                if (!isMarkedReference(next_m)) {
-                    // see if we have a match; using .acquire to make sure the payload is valid on this CPU core
-                    const prev_ref_count = current.ref_count.fetchAdd(1, .acquire);
-                    //
-                    if (prev_ref_count != 0) {
-                        if (match(&current.payload, args)) {
-                            // make sure the item hasn't been removed while we're checking it
-                            return &current.payload;
-                        }
-                    }
-                    _ = current.ref_count.fetchSub(1, .monotonic);
-                }
-                current = next;
-            }
-            return null;
-        }
-
-        pub fn addRef(_: *@This(), payload_ptr: *T) void {
-            const node: *Node = @fieldParentPtr("payload", payload_ptr);
-            node.ref_count.fetchAdd(1, .monotonic);
-        }
-
-        pub fn release(_: *@This(), payload_ptr: *T) void {
-            const node: *Node = @fieldParentPtr("payload", payload_ptr);
-            node.ref_count.fetchSub(1, .monotonic);
-        }
-
-        pub fn shift(self: *@This()) ?T {
-            var current = self.head.load(.unordered);
-            while (current != tail) {
-                const next_m = current.next.load(.unordered);
-                if (!isMarkedReference(next_m)) {
-                    // the next pointer isn't marked, meaning this node is on the list; mark the
-                    // pointer to take it off the list
-                    const new_next_m = getMarkedReference(next_m);
-                    if (current.next.cmpxchgWeak(next_m, new_next_m, .monotonic, .monotonic) == null) {
-                        // make sure payload is fully available
-                        _ = current.ref_count.load(.acquire);
-                        // make node available for reuse after copying is finished
-                        defer _ = current.ref_count.fetchSub(1, .monotonic);
-                        return current.payload;
-                    } else {
-                        // start from begining again
-                        break;
-                    }
-                }
-                current = getUnmarkedReference(next_m);
+                break;
             }
             return null;
         }
 
         pub fn deinit(self: *@This()) void {
-            var current = self.head.load(.unordered);
-            return while (current != tail) {
-                const next_m = current.next.load(.unordered);
-                self.allocator.destroy(current);
-                current = getUnmarkedReference(next_m);
-            };
-        }
-
-        inline fn isMarkedReference(ptr: *Node) bool {
-            return @intFromPtr(ptr) & 1 != 0;
-        }
-
-        inline fn getUnmarkedReference(ptr: *Node) *Node {
-            @setRuntimeSafety(false);
-            return @ptrFromInt(@intFromPtr(ptr) & ~@as(usize, 1));
-        }
-
-        inline fn getMarkedReference(ptr: *Node) *Node {
-            @setRuntimeSafety(false);
-            return @ptrFromInt(@intFromPtr(ptr) | @as(usize, 1));
+            var current = self.head.load();
+            while (!current.isNull()) {
+                const next = current.ptr().next.load();
+                self.allocator.destroy(current.ptr());
+                current = next;
+            }
         }
     };
 }
@@ -187,41 +200,66 @@ test "ListedList.push()" {
     var gpa = std.heap.DebugAllocator(.{}).init;
     var list: LinkedList(i32) = .init(gpa.allocator());
     defer list.deinit();
-    const ptr1 = try list.push(123);
-    try expectEqual(123, ptr1.*);
-    const ptr2 = try list.push(456);
-    try expectEqual(456, ptr2.*);
+    try list.push(123);
+    try list.push(456);
 }
 
 test "ListedList.shift()" {
     var gpa = std.heap.DebugAllocator(.{}).init;
     var list: LinkedList(i32) = .init(gpa.allocator());
     defer list.deinit();
-    _ = try list.push(123);
-    _ = try list.push(456);
-    const value1 = list.shift();
-    try expectEqual(123, value1);
-    const value2 = list.shift();
-    try expectEqual(456, value2);
-    const value3 = list.shift();
-    try expectEqual(null, value3);
-    _ = try list.push(888);
-    const value4 = list.shift();
-    try expectEqual(888, value4);
+    try list.push(123);
+    try list.push(456);
+    try expectEqual(123, list.shift());
+    try expectEqual(456, list.shift());
+    try expectEqual(null, list.shift());
+    try list.push(888);
+    try expectEqual(888, list.shift());
+    try list.push(111);
+    try list.push(222);
+    try list.push(333);
+    try expectEqual(111, list.shift());
+    try expectEqual(222, list.shift());
+    try list.push(444);
+    try list.push(555);
+    try expectEqual(333, list.shift());
+    try expectEqual(444, list.shift());
+    try expectEqual(555, list.shift());
+    try expectEqual(null, list.shift());
+}
+
+test "ListedList.remove()" {
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    var list: LinkedList(i32) = .init(gpa.allocator());
+    defer list.deinit();
+    const ns = struct {
+        fn match(self: i32, other: i32) bool {
+            return self == other;
+        }
+    };
+    try list.push(123);
+    try list.push(456);
+    try expectEqual(123, list.remove(ns.match, 123));
+    try expectEqual(null, list.remove(ns.match, 123));
+    try expectEqual(456, list.shift());
+    try expectEqual(null, list.remove(ns.match, 456));
 }
 
 test "ListedList.find()" {
     var gpa = std.heap.DebugAllocator(.{}).init;
     var list: LinkedList(i32) = .init(gpa.allocator());
     defer list.deinit();
-    _ = try list.push(123);
-    _ = try list.push(456);
     const ns = struct {
-        fn match(self: *i32, other: i32) bool {
-            return self.* == other;
+        fn match(self: i32, other: i32) bool {
+            return self == other;
         }
     };
-    const ptr = list.find(ns.match, 456);
-    try expect(ptr != null);
-    try expectEqual(456, ptr.?.*);
+    try list.push(123);
+    try list.push(456);
+    try expectEqual(123, list.find(ns.match, 123));
+    try expectEqual(123, list.find(ns.match, 123));
+    try expectEqual(123, list.shift());
+    try expectEqual(null, list.find(ns.match, 123));
+    try expectEqual(456, list.shift());
+    try expectEqual(null, list.find(ns.match, 456));
 }
