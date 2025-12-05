97a9d4d34c4af076a3ab29259fd916767e8d1727
Chung Leong <chernyshevsky@hotmail.com>
2025-12-05 17:09:13 +0100

    Fixed tests on MacOS (#825)

--- zigar-compiler/test/integration/stream-handling/stat-opened-file-in-file-system-with-posix-function.zig
+++ zigar-compiler/test/integration/stream-handling/stat-opened-file-in-file-system-with-posix-function.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const c = @cImport({
     @cInclude("sys/stat.h");
@@ -6,12 +7,19 @@ const c = @cImport({
     @cInclude("fcntl.h");
 });
 
+const darwin = struct {
+    // translate-c currently doesn't handle the __DARWIN_INODE64 macro
+    extern fn @"fstat$INODE64"(c_int, [*c]c.struct_stat) c_int;
+};
+
+const fstat = if (builtin.target.os.tag.isDarwin()) darwin.@"fstat$INODE64" else c.fstat;
+
 pub fn print(path: [*:0]const u8) !void {
     const fd = c.open(path, c.O_RDONLY);
     if (fd < 0) return error.UnableToOpenFile;
     defer _ = c.close(fd);
     var info: c.struct_stat = undefined;
-    const result = c.fstat(fd, &info);
+    const result = fstat(fd, &info);
     if (result != 0) return error.UnableToGetStat;
     std.debug.print("size = {d}\n", .{info.st_size});
 }
