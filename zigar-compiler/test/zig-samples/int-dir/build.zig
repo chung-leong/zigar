ad7bdfbe70ef9ef1fdd9209f67f810aa6d2bd0cb
Chung Leong <chernyshevsky@hotmail.com>
2025-12-08 02:01:02 +0100

    Updated tests

--- zigar-compiler/test/zig-samples/int-dir/build.zig
+++ zigar-compiler/test/zig-samples/int-dir/build.zig
@@ -6,42 +6,73 @@ const cfg = @import("build.cfg.zig");
 const extra = @import("build.extra.zig");
 
 pub fn build(b: *std.Build) !void {
-    if (builtin.zig_version.major != 0 or builtin.zig_version.minor != 14) {
+    if (builtin.zig_version.major != 0 or builtin.zig_version.minor != 15) {
         @compileError("Unsupported Zig version");
     }
-    const host_type = if (cfg.is_wasm) "wasm" else "napi";
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const build_args = .{ .target = target, .optimize = optimize };
-    const lib = b.addSharedLibrary(.{
+    const use_llvm = @as(?bool, cfg.use_llvm) orelse default: {
+        if (cfg.is_wasm) break :default true;
+        if (builtin.target.cpu.arch == .x86_64 and cfg.multithreaded) break :default true;
+        break :default null;
+    };
+    const lib = b.addLibrary(.{
+        .linkage = .dynamic,
         .name = cfg.module_name,
-        .root_source_file = .{ .cwd_relative = cfg.zigar_src_path ++ "stub-" ++ host_type ++ ".zig" },
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = !cfg.multithreaded,
+        .root_module = b.addModule("root", .{
+            .root_source_file = .{ .cwd_relative = cfg.zigar_src_path ++ "stub.zig" },
+            .target = target,
+            .optimize = optimize,
+            .single_threaded = !cfg.multithreaded,
+        }),
+        .use_llvm = use_llvm,
     });
     const zigar = b.createModule(.{
         .root_source_file = .{ .cwd_relative = cfg.zigar_src_path ++ "zigar.zig" },
     });
     const zigar_imports: []const Import = &.{.{ .name = "zigar", .module = zigar }};
     const extra_imports: []const Import = switch (@hasDecl(extra, "getImports")) {
-        true => @call(.always_inline, extra.getImports, .{ b, build_args }),
+        true => @call(.always_inline, extra.getImports, .{ b, .{
+            .library = lib,
+            .target = target,
+            .optimize = optimize,
+        } }),
         false => &.{},
     };
     const imports = try std.mem.concat(b.allocator, Import, &.{ zigar_imports, extra_imports });
     const mod = b.createModule(.{
         .root_source_file = .{ .cwd_relative = cfg.module_dir ++ "integers.zig" },
+        .target = target,
+        .optimize = optimize,
         .imports = imports,
     });
     mod.addIncludePath(.{ .cwd_relative = cfg.module_dir });
     lib.root_module.addImport("module", mod);
     const extra_c_files: []const []const u8 = switch (@hasDecl(extra, "getCSourceFiles")) {
-        true => @call(.always_inline, extra.getCSourceFiles, .{ b, build_args }),
+        true => @call(.always_inline, extra.getCSourceFiles, .{ b, .{
+            .library = lib,
+            .module = mod,
+            .target = target,
+            .optimize = optimize,
+        } }),
         false => &.{},
     };
     for (extra_c_files) |file| {
-        const path = try std.mem.concat(b.allocator, u8, &.{ cfg.module_dir, file });
+        const path = try std.fs.path.resolve(b.allocator, &.{ cfg.module_dir, file });
         lib.addCSourceFile(.{ .file = .{ .cwd_relative = path } });
+    }
+    const extra_include_paths: []const []const u8 = switch (@hasDecl(extra, "getIncludePaths")) {
+        true => @call(.always_inline, extra.getIncludePaths, .{ b, .{
+            .library = lib,
+            .module = mod,
+            .target = target,
+            .optimize = optimize,
+        } }),
+        false => &.{},
+    };
+    for (extra_include_paths) |inc_path| {
+        const path = try std.fs.path.resolve(b.allocator, &.{ cfg.module_dir, inc_path });
+        lib.addIncludePath(.{ .file = .{ .cwd_relative = path } });
     }
     if (cfg.use_libc) {
         lib.linkLibC();
@@ -53,19 +84,20 @@ pub fn build(b: *std.Build) !void {
         lib.entry = .disabled;
         lib.rdynamic = true;
         lib.wasi_exec_model = .reactor;
-        lib.import_memory = true;
-        lib.import_table = true;
+        lib.import_memory = cfg.multithreaded;
+        lib.import_table = !cfg.multithreaded;
         lib.stack_size = cfg.stack_size;
         lib.max_memory = cfg.max_memory;
     } else if (cfg.use_redirection) {
-        lib.addCSourceFile(.{ .file = .{ .cwd_relative = cfg.zigar_src_path ++ "hooks.c" } });
+        lib.addCSourceFile(.{ .file = .{ .cwd_relative = cfg.zigar_src_path ++ "host/native/hooks.c" } });
     }
     const options = b.addOptions();
     options.addOption(comptime_int, "eval_branch_quota", cfg.eval_branch_quota);
     options.addOption(bool, "omit_functions", cfg.omit_functions);
     options.addOption(bool, "omit_variables", cfg.omit_variables);
     options.addOption(bool, "use_redirection", cfg.use_redirection);
-    lib.root_module.addOptions("./export-options.zig", options);
+    options.addOption(bool, "use_pthread_emulation", cfg.use_pthread_emulation);
+    lib.root_module.addOptions("options.zig", options);
     const wf = b.addUpdateSourceFiles();
     wf.addCopyFileToSource(lib.getEmittedBin(), cfg.output_path);
     if (@TypeOf(cfg.pdb_path) != @TypeOf(null) and optimize == .Debug) {
@@ -73,4 +105,8 @@ pub fn build(b: *std.Build) !void {
     }
     wf.step.dependOn(&lib.step);
     b.getInstallStep().dependOn(&wf.step);
+    const mod_tests = b.addTest(.{ .root_module = mod });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
 }
