fbc29ab2c23d559843d335cf3ca5e23c409f835a
Chung Leong <chernyshevsky@hotmail.com>
2026-01-31 18:42:59 +0100

    Began writing proper test cases for php-zigar (#838)

--- php-zigar/tests/ZigImporter.php
+++ php-zigar/tests/ZigImporter.php
@@ -2,9 +2,12 @@
 
 final class ZigImporter 
 {
-    public static function load($path) 
+    public static function load($src_path) 
     {
-        // TODO
+        $info = pathinfo($src_path);
+        $mod_path = "{$info['dirname']}/lib/{$info['filename']}.zigar";
+        zigar_compile_module($src_path, $mod_path);
+        return zigar_load_module($mod_path);
     }
 }
 
--- php-zigar/tests/type-handling/bool/BoolHandlingTest.php
+++ php-zigar/tests/type-handling/bool/BoolHandlingTest.php
@@ -5,73 +5,88 @@ final class BoolHandlingTest extends TestCase
 {   
     public function testImportBoolAsStaticVariables(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/as-static-variables.zig');
-        $this->assertSame("A", "B");
+        $m = ZigImporter::load(__DIR__ . '/as-static-variables.zig');
+        $this->assertSame(true, $m->bool1);
+        $this->assertSame(false, $m->bool2);
+        $this->assertSame(true, $m->bool3);
+        $this->assertSame(false, $m->bool4);
+
+        $m->bool1 = false;
+        $this->assertSame(false, $m->bool1);
+
+        $this->expectExceptionMessage("write protected (zig)");
+        $m->bool4 = false;
+
+        $this->expectOutputString('no');
+        $m->print();
     }
 
     public function testPrintBoolArguments(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/as-function-parameters.zig');
-        $this->assertSame("B", "B");
+        $m = ZigImporter::load(__DIR__ . '/as-function-parameters.zig');
+        $this->expectOutputString("no\nyes\n");
+        $m->print(false);
+        $m->print(true);
     }
 
     public function testReturnBool(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/as-return-value.zig');
-        $this->assertSame("B", "B");
+        $m = ZigImporter::load(__DIR__ . '/as-return-value.zig');
+        $this->assertSame(false, $m->getFalse());
+        $this->assertSame(true, $m->getTrue());
     }
 
     public function testHandleBoolInArray(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/array-of.zig');
+        $m = ZigImporter::load(__DIR__ . '/array-of.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInStruct(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-struct.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-struct.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInPackedStruct(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-packed-struct.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-packed-struct.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolAsComptimeField(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/as-comptime-field.zig');
+        $m = ZigImporter::load(__DIR__ . '/as-comptime-field.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInBareUnion(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-bare-union.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-bare-union.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInTaggedUnion(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-tagged-union.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-tagged-union.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInOptional(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-optional.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-optional.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInErrorUnion(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/in-error-union.zig');
+        $m = ZigImporter::load(__DIR__ . '/in-error-union.zig');
         $this->assertSame("B", "B");
     }
 
     public function testHandleBoolInVector(): void
     {
-        $module = ZigImporter::load(__DIR__ . '/vector-of.zig');
+        $m = ZigImporter::load(__DIR__ . '/vector-of.zig');
         $this->assertSame("B", "B");
     }   
 }
