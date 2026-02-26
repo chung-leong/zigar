310617b0aa1f21106aa64a19291fbdaa52946e3a
Chung Leong <chernyshevsky@hotmail.com>
2026-02-26 23:22:54 +0100

    More tests for comptime_int

--- php-zigar/tests/type-handling/comptime-int/ComptimeIntHandlingTest.php
+++ php-zigar/tests/type-handling/comptime-int/ComptimeIntHandlingTest.php
@@ -31,18 +31,26 @@ final class ComptimeIntHandlingTest extends TestCase
     public function testHandleComptimeIntInArray(): void
     {
         $m = ZigImporter::load(__DIR__ . '/array-of.zig');
-        $this->assertSame(1, $m->array1[0]);
-        $this->assertSame(2, $m->array1[1]);
-        $this->assertSame(3, $m->array1[2]);
-        $this->assertSame(4, $m->array1[3]);
-        $this->assertSame(0x1000_0000_0000_0000, $m->array2[0]);
-        $this->assertSame(0x2000_0000_0000_0000, $m->array2[1]);
+        $this->assertSame([ 1, 2, 3, 4 ], (array) $m->array1);
+        // $this->assertSame(0x1000_0000_0000_0000, $m->array2[0]);
+        // $this->assertSame(0x2000_0000_0000_0000, $m->array2[1]);
     }
 
     public function testHandleComptimeIntInStruct(): void
     {
         $m = ZigImporter::load(__DIR__ . '/in-struct.zig');
-        $this->assertSame("B", "B");
+        $this->assertSame([ 'number1' => 1, 'number2' => 2 ], (array) $m->struct_a);
+
+        $this->expectExceptionMessage("write protected (zig)");
+        $x = new $m->StructA(number1: 1);
+
+        $b = new $m->StructA([]);
+        $this->assertSame([ 'number1' => 100, 'number2' => 200 ], (array) $b);
+        $this->expectOutputString(<<<OUTPUT
+        .{ .number1 = 1, .number2 = 2 }
+
+        OUTPUT);
+        $m->print();
     }
 
     public function testHandleComptimeIntInPackedStruct(): void
@@ -54,19 +62,30 @@ final class ComptimeIntHandlingTest extends TestCase
     public function testHandleComptimeIntAsComptimeField(): void
     {
         $m = ZigImporter::load(__DIR__ . '/as-comptime-field.zig');
-        $this->assertSame("B", "B");
+        $this->assertSame(1234, $m->struct_a->number);
+        $b = new $m->StructA(state: false);
+        $this->assertSame([ 'number' => 1234, 'state' => false ], (array) $b);
     }
 
     public function testHandleComptimeIntInBareUnion(): void
     {
+        $this->expectExceptionMessage("unable to create module");
         $m = ZigImporter::load(__DIR__ . '/in-bare-union.zig');
-        $this->assertSame("B", "B");
     }
 
     public function testHandleComptimeIntInTaggedUnion(): void
     {
         $m = ZigImporter::load(__DIR__ . '/in-tagged-union.zig');
-        $this->assertSame("B", "B");
+        $this->assertSame(123, $m->union_a->number);
+        $this->assertSame([ 'number' => 123 ], (array) $m->union_a);
+        $tag = $m->TagType($m->union_a);
+        $this->assertSame('number', (string) $tag);
+
+        $b = new $m->UnionA(state: true);
+        $this->assertSame([ 'state' => true ], (array) $b);
+
+        $this->expectExceptionMessage("write protected (zig)");
+        $x = new $m->UnionA(number: 0);
     }
 
     public function testHandleComptimeIntInOptional(): void
@@ -79,7 +98,9 @@ final class ComptimeIntHandlingTest extends TestCase
     public function testHandleComptimeIntInErrorUnion(): void
     {
         $m = ZigImporter::load(__DIR__ . '/in-error-union.zig');
-        $this->assertSame("B", "B");
+        $this->assertSame(1234, $m->error_union1);
+        $this->expectExceptionMessage("goldfish died");
+        $x = $m->error_union2;
     }
 
     public function testHandleComptimeIntInVector(): void
@@ -87,5 +108,11 @@ final class ComptimeIntHandlingTest extends TestCase
         $this->expectExceptionMessage("unable to create module");
         $m = ZigImporter::load(__DIR__ . '/vector-of.zig');
     }   
+
+    public function testConstructComptimeInt(): void
+    {
+        $m = ZigImporter::load(__DIR__ . '/constructor.zig');
+        $this->assertSame(false, isset($m->ComptimeInt));
+    }
 }
 
