8eab923bdf57b61e55049fba7a64b5005cde2c55
Chung Leong <chernyshevsky@hotmail.com>
2026-06-08 11:40:44 +0200

    Fixed leak

--- php-zigar/src/buffer.zig
+++ php-zigar/src/buffer.zig
@@ -148,6 +148,7 @@ pub const ByteBuffer = struct {
             .flags = self.flags,
             .source = .{ .buffer = src_buf },
         };
+        new.flags.temporary = false;
         return new;
     }
 
