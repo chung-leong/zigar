702fbc76b5a10f5008056c3c54d8919d626bb80e
Chung Leong <chernyshevsky@hotmail.com>
2026-07-30 00:24:35 +0200

    Fixed pointer in packed struct (#1035)

--- zigar-compiler/test/integration/type-handling/pointer/in-packed-struct.zig
+++ zigar-compiler/test/integration/type-handling/pointer/in-packed-struct.zig
@@ -3,15 +3,20 @@ const std = @import("std");
 pub var number: i32 = 1234;
 
 pub const StructA = packed struct {
-    ptr1: *i32 = &number,
-    ptr2: *i32 = &number,
-    number: u10 = 100,
-    ptr3: *i32 = &number,
+    ptr1: *i32,
+    ptr2: *i32,
+    number: u10,
+    ptr3: *i32,
 };
 
 // struct cannot be initialized since non-byte-aligned relocation is not allowed
 pub var struct_a: StructA = undefined;
 
 pub fn init() void {
-    struct_a = .{ .number = 200 };
+    struct_a = .{
+        .ptr1 = &number,
+        .ptr2 = &number,
+        .ptr3 = &number,
+        .number = 200,
+    };
 }
