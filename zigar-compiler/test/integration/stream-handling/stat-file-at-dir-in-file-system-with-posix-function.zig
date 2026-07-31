511929b632a4dc30a616e41cb10c64cc107ca30a
Chung Leong <chernyshevsky@hotmail.com>
2026-07-31 13:53:49 +0200

    Removed workaround for Darwin in test samples

--- zigar-compiler/test/integration/stream-handling/stat-file-at-dir-in-file-system-with-posix-function.zig
+++ zigar-compiler/test/integration/stream-handling/stat-file-at-dir-in-file-system-with-posix-function.zig
@@ -3,17 +3,10 @@ const builtin = @import("builtin");
 
 const c = @import("c");
 
-const darwin = struct {
-    // translate-c currently doesn't handle the __DARWIN_INODE64 macro
-    extern fn @"fstatat$INODE64"(c_int, [*c]const u8, [*c]c.struct_stat, c_int) c_int;
-};
-
-const fstatat = if (builtin.target.os.tag.isDarwin()) darwin.@"fstatat$INODE64" else c.fstatat;
-
 pub fn stat(dir_path: [*:0]const u8, path: [*:0]const u8) !void {
     const dirfd = c.openat(c.AT_FDCWD, dir_path, c.O_DIRECTORY | c.O_RDONLY);
     if (dirfd < 0) return error.UnableToOpenDirectory;
     var info: c.struct_stat = undefined;
-    if (fstatat(dirfd, path, &info, 0) != 0) return error.UnableToGetStat;
+    if (c.fstatat(dirfd, path, &info, 0) != 0) return error.UnableToGetStat;
     std.debug.print("size = {d}\n", .{info.st_size});
 }
