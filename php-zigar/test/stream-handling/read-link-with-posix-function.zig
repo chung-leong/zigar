a512ff6227bf0d239b1df48117480b1c5d13635f
Chung Leong <chernyshevsky@hotmail.com>
2026-08-13 02:00:52 +0200

    Fixed tests (#1052)

--- php-zigar/test/stream-handling/read-link-with-posix-function.zig
+++ php-zigar/test/stream-handling/read-link-with-posix-function.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
-pub fn readLink(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
+pub fn readLink(allocator: std.mem.Allocator, path: [:0]const u8) ![]const u8 {
     var buffer: [4096]u8 = undefined;
-    const result = try std.posix.readlink(path, &buffer);
-    return allocator.dupe(u8, result);
+    const result = std.c.readlink(path.ptr, &buffer, buffer.len);
+    if (result < 0) return error.UnableToReadLink;
+    const len: usize = @intCast(result);
+    return allocator.dupe(u8, buffer[0..len]);
 }
