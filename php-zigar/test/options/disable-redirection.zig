9e0c13efcddfe86abf8e83acdea8ee5df990e810
Chung Leong <chernyshevsky@hotmail.com>
2026-08-21 02:50:10 +0200

    Fixed test

--- php-zigar/test/options/disable-redirection.zig
+++ php-zigar/test/options/disable-redirection.zig
@@ -1,7 +1,10 @@
 const std = @import("std");
+var thread_io = std.Io.Threaded.init_single_threaded;
+
+const io = thread_io.io();
 
 pub fn check(path: []const u8) bool {
-    var file = std.fs.openFileAbsolute(path, .{}) catch return false;
-    defer file.close();
+    var file = std.Io.Dir.openFileAbsolute(io, path, .{}) catch return false;
+    defer file.close(io);
     return true;
 }
