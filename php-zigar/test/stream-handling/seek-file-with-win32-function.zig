62bd7e0e1f1a0da69e7d1826d35d68abc534953f
Chung Leong <chernyshevsky@hotmail.com>
2026-08-26 01:37:11 +0200

    Fixed test (#1062)

--- php-zigar/test/stream-handling/seek-file-with-win32-function.zig
+++ php-zigar/test/stream-handling/seek-file-with-win32-function.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
+var threaded_io = std.Io.Threaded.init_single_threaded;
 
 const c = @import("c");
+
+const io = threaded_io.io();
 
 pub fn read(allocator: std.mem.Allocator, file: std.Io.File, offset: usize, len: usize) ![]u8 {
     if (c.SetFilePointer(file.handle, @intCast(offset), null, c.FILE_BEGIN) == 0) {
         return error.UnableToSeekFile;
     }
     const buffer: []u8 = try allocator.alloc(u8, len);
-    const bytes_read = try file.read(buffer);
+    const slices: [1][]u8 = .{buffer};
+    const bytes_read = try file.readStreaming(io, &slices);
     return buffer[0..bytes_read];
 }
