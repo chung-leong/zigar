5c133565f4413bdc1d238bacc923902fe3e81dfe
Chung Leong <chernyshevsky@hotmail.com>
2026-08-28 17:22:21 +0200

    Fixed win32 intrinsics

--- php-zigar/src/include/win32/php_config.h
+++ php-zigar/src/include/win32/php_config.h
@@ -1,6 +1,6 @@
 #ifndef PHP_CONFIG_H
 #define PHP_CONFIG_H
-#include "msvc_intrinsics.h"
+#include <intrin.h>
 
 #define ZEND_API __declspec(dllimport)
 #define ZEND_DLEXPORT __declspec(dllexport)
@@ -11,5 +11,22 @@
 
 #define _pid_t int
 #define _PID_T_
+
+char _InterlockedExchange8(
+   char volatile * Target,
+   char Value
+);
+char _InterlockedExchange8_acq(
+   char volatile * Target,
+   char Value
+);
+char _InterlockedExchange8_nf(
+   char volatile * Target,
+   char Value
+);
+char _InterlockedExchange8_rel(
+   char volatile * Target,
+   char Value
+);
 
 #endif 
