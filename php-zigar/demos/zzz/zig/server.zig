60fde4245bed6037d8a82cb235a2418c6e4aaa66
Chung Leong <chernyshevsky@hotmail.com>
2026-09-06 23:11:46 +0200

    Updated zzz example again (#1078)

--- php-zigar/demos/zzz/zig/server.zig
+++ php-zigar/demos/zzz/zig/server.zig
@@ -84,6 +84,7 @@ fn main(host: []const u8, port: u16, promise: zigar.function.Promise(anyerror!vo
 
     var router = try Router.init(allocator, &.{
         Route.init("/cat").get({}, cat_handler).layer(),
+        Route.init("/").get({}, base_handler).layer(),
         Route.init("/%r").get({}, base_handler).layer(),
     }, .{});
     defer router.deinit(allocator);
